84cb3d58b20b829174e25168e29463344e664362
Praveen Yadav <pyadav9678@gmail.com>
2020-10-12 09:12:08 +0530

    fix: implemente vtable into apasara-ui

--- packages/apsara-base/src/index.ts
+++ packages/apsara-base/src/index.ts
@@ -3,9 +3,10 @@ import Icon from "./Icon";
 import Listing from "./Listing";
 import Search from "./Search";
 import Table from "./Table";
+import VTable from "./Table/VirtualisedTable";
 import Text from "./Text";
 
 import DEFAULT_THEME from "./theme";
 
 import "../styles/app.less";
-export { DEFAULT_THEME, Button, Icon, Listing, Search, Table, Text };
+export { DEFAULT_THEME, Button, Icon, Listing, Search, Table, VTable, Text };
