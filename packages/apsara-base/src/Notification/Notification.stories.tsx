386227e475cf19f9d6eef14f55d50e176fe41468
Rishabh Mishra <me@rishabhmishra.dev>
2020-11-25 22:04:40 +0530

    feat: add notifications component

---
+++ packages/apsara-base/src/Notification/Notification.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "../Button";
+import { showSuccess, showError } from "./Notification";
+
+export default {
+    title: "General/Notifications",
+};
+
+export const notifications = () => {
+    const handleSuccessClick = () => {
+        showSuccess("Success", "This is test");
+    };
+    const handleErrorClick = () => {
+        showError("Error", "This is test");
+    };
+    return (
+        <>
+            <Button onClick={handleSuccessClick}>show Success</Button>
+            <Button onClick={handleErrorClick}>show Error</Button>
+        </>
+    );
+};
--- packages/apsara-base/src/Notification/Notification.tsx
+++ packages/apsara-base/src/Notification/Notification.tsx
@@ -1,1 +1,27 @@
 export { notification } from "antd";
+import React from "react";
+import Theme from "../../theme";
+import { notification } from "antd";
+import Icon from "../Icon";
+
+const CrossIcon = <Icon name="cross" styleOverride={{ color: Theme["@datlantis-close-icon-color"] }} />;
+
+export const showError = (title: string, content?: string) => {
+    notification.error({
+        message: title,
+        description: content || "",
+        placement: "bottomRight",
+        icon: <Icon name="error" size={32} styleOverride={{ color: Theme["@red-6"] }} />,
+        closeIcon: CrossIcon,
+    });
+};
+
+export const showSuccess = (title: string, content?: string) => {
+    notification.success({
+        message: title,
+        description: content,
+        placement: "bottomRight",
+        icon: <Icon name="checkcircle" size={32} styleOverride={{ color: Theme["@green-6"] }} />,
+        closeIcon: CrossIcon,
+    });
+};
