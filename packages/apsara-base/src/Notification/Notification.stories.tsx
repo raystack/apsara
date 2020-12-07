15394598126330e3fc3271c740630d64a81685a7
Rishabh Mishra <me@rishabhmishra.dev>
2020-12-07 22:55:25 +0530

    feat: add custom notification with footer

--- packages/apsara-base/src/Notification/Notification.stories.tsx
+++ packages/apsara-base/src/Notification/Notification.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button";
-import { showSuccess, showError } from "./Notification";
+import { showSuccess, showError, showNotification } from "./Notification";
+import Icon from "../Icon";
 
 export default {
     title: "Feedback/Notifications",
@@ -13,10 +14,29 @@ export const notifications = () => {
     const handleErrorClick = () => {
         showError("Error", "This is test");
     };
+    const handleCustomClick = () => {
+        showNotification({
+            title: "Alerts Configured",
+            icon: <Icon name="copy2" />,
+            content:
+                "When you offer to subscribe to browser notifications for your users, it is very important to do so delicately and preferably in two steps.",
+            footer: (
+                <div style={{ display: "flex" }}>
+                    <Button type="barebone" iconName="copy2">
+                        Button 1
+                    </Button>
+                    <Button type="barebone" iconName="copy2">
+                        Button 2
+                    </Button>
+                </div>
+            ),
+        });
+    };
     return (
         <>
             <Button onClick={handleSuccessClick}>show Success</Button>
             <Button onClick={handleErrorClick}>show Error</Button>
+            <Button onClick={handleCustomClick}>custom notification with action</Button>
         </>
     );
 };
--- packages/apsara-base/src/Notification/Notification.tsx
+++ packages/apsara-base/src/Notification/Notification.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Theme from "../theme";
 import { notification } from "antd";
 import Icon from "../Icon";
+import "./style.less";
 
 const CrossIcon = <Icon name="cross" styleOverride={{ color: Theme["@datlantis-close-icon-color"] }} />;
 
@@ -23,5 +24,34 @@ export const showSuccess = (title: string, content?: string) => {
         placement: "bottomRight",
         icon: <Icon name="checkcircle" size={32} styleOverride={{ color: Theme["@green-6"] }} />,
         closeIcon: CrossIcon,
+    });
+};
+
+interface NotificationProps {
+    title: string;
+    content?: string;
+    icon?: React.ReactNode;
+    footer?: React.ReactNode;
+}
+
+export const showNotification = ({
+    title = "",
+    content = "",
+    icon = <Icon name="checkcircle" size={32} styleOverride={{ color: Theme["@green-6"] }} />,
+    footer = null,
+}: NotificationProps) => {
+    const description = (
+        <>
+            <div className="custom-notification__content">{content}</div>
+            {footer}
+        </>
+    );
+    notification.open({
+        message: title,
+        description,
+        placement: "bottomRight",
+        icon,
+        closeIcon: CrossIcon,
+        className: "custom-notification",
     });
 };
