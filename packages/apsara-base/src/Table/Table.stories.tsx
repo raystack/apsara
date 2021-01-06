655aedb67d249978782463e16178eb6cf2fdbb61
Rishabh Mishra <me@rishabhmishra.dev>
2021-01-06 23:13:30 +0530

    feat(table): add support for infinite list in virtual table

--- packages/apsara-base/src/Table/Table.stories.tsx
+++ packages/apsara-base/src/Table/Table.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Table from "./Table";
 import VTable from "./VirtualisedTable";
@@ -10,14 +10,19 @@ export default {
 
 export const TableWithoutData = () => <Table items={[]} />;
 
-const dataSource = new Array(100).fill(0).map((_, index) => {
-    return {
-        key: index,
-        name: `name ${index}`,
-        age: 32,
-        address: "10 Downing Street",
-    };
-});
+function getData(page = 1) {
+    return new Array(page * 100).fill(0).map((_, index) => {
+        return {
+            key: index,
+            name: `name ${index}`,
+            age: index,
+            address: "10 Downing Street",
+        };
+    });
+}
+
+const dataSource = getData();
+
 const columns = [
     {
         title: "Name",
@@ -36,5 +41,13 @@ const columns = [
     },
 ];
 
-export const TableWithData = () => <Table items={dataSource} columns={columns} />;
+export const TableWithData = () => <Table items={dataSource} columns={columns} scroll={{ x: true, y: 300 }} />;
 export const VirtualTableWithData = () => <VTable items={dataSource} columns={columns} />;
+
+export const VirtualTableWithInfiniteData = () => {
+    const [page, setPage] = useState(1);
+    function loadMore() {
+        setPage(page + 1);
+    }
+    return <VTable items={getData(page)} columns={columns} loadMore={loadMore} />;
+};
--- packages/apsara-base/src/Table/VirtualisedTable.tsx
+++ packages/apsara-base/src/Table/VirtualisedTable.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { VariableSizeGrid as Grid } from "react-window";
 import ResizeObserver from "rc-resize-observer";
 import { TableProps } from "antd/lib/table";
-
+import InfiniteLoader from "react-window-infinite-loader";
 import Table from "./Table";
 
 const DEFAULT_HEIGHT = 700;
@@ -12,6 +12,7 @@ interface IVirtualTable extends TableProps<any> {
     items: any[];
     selectedRowId?: number | null;
     scroll?: any;
+    loadMore?: () => void;
     onRowClick?: (event: any, rowIndexData: any) => null;
 }
 const VirtualTableComponent = ({
@@ -19,10 +20,11 @@ const VirtualTableComponent = ({
     scroll,
     selectedRowId = null,
     onRowClick = () => null,
+    loadMore = () => null,
     ...props
 }: IVirtualTable) => {
     const [tableWidth, setTableWidth] = useState(0);
-
+    const [lastIndex, setLastIndex] = useState(0);
     const widthColumnCount = columns.filter(({ width }) => !width).length;
     const totalColumnDefinedWidth = columns.reduce((acc, { width = 0 }) => acc + width, 0);
     const mergedColumns = columns.map((column) => {
@@ -64,49 +66,75 @@ const VirtualTableComponent = ({
     const renderVirtualList = (rawData: Record<string, unknown>[], { scrollbarSize, ref, onScroll }: any) => {
         // eslint-disable-next-line no-param-reassign
         ref.current = connectObject;
-        return (
-            <Grid
-                ref={gridRef}
-                className="virtual-grid"
-                columnCount={mergedColumns.length}
-                columnWidth={(index) => {
-                    const { width } = mergedColumns[index];
-                    return index === mergedColumns.length - 1 ? width - scrollbarSize - 1 : width;
-                }}
-                height={scroll.y}
-                rowCount={rawData.length}
-                rowHeight={() => 54}
-                width={tableWidth}
-                onScroll={({ scrollLeft }) => {
-                    onScroll({
-                        scrollLeft,
-                    });
-                }}
-            >
-                {({ columnIndex, rowIndex, style }) => {
-                    const columnIndexData = mergedColumns[columnIndex];
-                    const rowIndexData = rawData[rowIndex];
+        const rowCount = rawData.length;
+        const isItemLoaded = (index: number) => index < lastIndex;
 
-                    return (
-                        <div
-                            role="presentation"
-                            className={`virtual-table-cell ${
-                                columnIndex === mergedColumns.length - 1 ? "virtual-table-cell-last" : ""
-                            } ${rowIndex % 2 === 0 ? "virtual-table-even" : "virtual-table-odd"} ${
-                                columnIndex === 0 ? "virtual-table-first-child" : ""
-                            } 
-              ${columnIndex === mergedColumns.length - 1 ? "virtual-table-last-child" : ""} 
-              ${rowIndexData?.id === selectedRowId ? "highlightRow" : ""}`}
-                            style={style}
-                            onClick={(event) => onRowClick(event, rowIndexData)}
-                        >
-                            {columnIndexData?.render
-                                ? columnIndexData.render(rowIndexData[columnIndexData.dataIndex], rowIndexData)
-                                : rowIndexData[columnIndexData.dataIndex]}
-                        </div>
-                    );
-                }}
-            </Grid>
+        function loadMoreItems(_startIndex: number, stopIndex: number) {
+            if (stopIndex === rowCount - 1) {
+                loadMore();
+            }
+            setLastIndex(stopIndex);
+        }
+
+        return (
+            <InfiniteLoader isItemLoaded={isItemLoaded} itemCount={rowCount} loadMoreItems={loadMoreItems}>
+                {({ onItemsRendered }) => (
+                    <Grid
+                        onItemsRendered={({
+                            visibleRowStartIndex,
+                            visibleRowStopIndex,
+                            overscanRowStartIndex,
+                            overscanRowStopIndex,
+                        }) =>
+                            onItemsRendered({
+                                visibleStartIndex: visibleRowStartIndex,
+                                visibleStopIndex: visibleRowStopIndex,
+                                overscanStartIndex: overscanRowStartIndex,
+                                overscanStopIndex: overscanRowStopIndex,
+                            })
+                        }
+                        ref={gridRef}
+                        className="virtual-grid"
+                        columnCount={mergedColumns.length}
+                        columnWidth={(index) => {
+                            const { width } = mergedColumns[index];
+                            return index === mergedColumns.length - 1 ? width - scrollbarSize - 1 : width;
+                        }}
+                        height={scroll.y}
+                        rowCount={rowCount}
+                        rowHeight={() => 54}
+                        width={tableWidth}
+                        onScroll={({ scrollLeft }) => {
+                            onScroll({
+                                scrollLeft,
+                            });
+                        }}
+                    >
+                        {({ columnIndex, rowIndex, style }) => {
+                            const columnIndexData = mergedColumns[columnIndex];
+                            const rowIndexData = rawData[rowIndex];
+                            return (
+                                <div
+                                    role="presentation"
+                                    className={`virtual-table-cell ${
+                                        columnIndex === mergedColumns.length - 1 ? "virtual-table-cell-last" : ""
+                                    } ${rowIndex % 2 === 0 ? "virtual-table-even" : "virtual-table-odd"} ${
+                                        columnIndex === 0 ? "virtual-table-first-child" : ""
+                                    }
+                              ${columnIndex === mergedColumns.length - 1 ? "virtual-table-last-child" : ""}
+                              ${rowIndexData?.id === selectedRowId ? "highlightRow" : ""}`}
+                                    style={style}
+                                    onClick={(event) => onRowClick(event, rowIndexData)}
+                                >
+                                    {columnIndexData?.render
+                                        ? columnIndexData.render(rowIndexData[columnIndexData.dataIndex], rowIndexData)
+                                        : rowIndexData[columnIndexData.dataIndex]}
+                                </div>
+                            );
+                        }}
+                    </Grid>
+                )}
+            </InfiniteLoader>
         );
     };
     return (
