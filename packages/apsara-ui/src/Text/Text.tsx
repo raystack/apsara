9fe1b5c95194f69f1c23fdbb32edbc81a5f78b4a
Praveen Yadav <pyadav9678@gmail.com>
2021-03-09 09:08:34 +0530

    fix: lineheight for texts

--- packages/apsara-ui/src/Text/Text.tsx
+++ packages/apsara-ui/src/Text/Text.tsx
@@ -6,77 +6,77 @@ import styled from "styled-components";
 const styleMap = {
     11: {
         fontSize: "11px",
-        lineHeight: "15px",
+        lineHeight: "13px",
         letterSpacing: "0.11px",
     },
     12: {
         fontSize: "12px",
-        lineHeight: "16px",
+        lineHeight: "14px",
         letterSpacing: "0.12px",
     },
     14: {
         fontSize: "14px",
-        lineHeight: "19px",
+        lineHeight: "16px",
         letterSpacing: "0.14px",
     },
     16: {
         fontSize: "16px",
-        lineHeight: "21px",
+        lineHeight: "19px",
         letterSpacing: "0.16px",
     },
     18: {
         fontSize: "18px",
-        lineHeight: "24px",
+        lineHeight: "21px",
         letterSpacing: "0.18px",
     },
     20: {
         fontSize: "20px",
-        lineHeight: "27px",
+        lineHeight: "24px",
         letterSpacing: "0.20px",
     },
     22: {
         fontSize: "22px",
-        lineHeight: "29px",
+        lineHeight: "25px",
         letterSpacing: "0.22px",
     },
     25: {
         fontSize: "25px",
-        lineHeight: "33px",
+        lineHeight: "29px",
         letterSpacing: "0.25px",
     },
     28: {
         fontSize: "28px",
-        lineHeight: "37px",
+        lineHeight: "33px",
         letterSpacing: "0.28px",
     },
     32: {
         fontSize: "32px",
-        lineHeight: "43px",
+        lineHeight: "38px",
         letterSpacing: "0.32px",
     },
     36: {
         fontSize: "36px",
-        lineHeight: "48px",
+        lineHeight: "42px",
         letterSpacing: "0.36px",
     },
     40: {
         fontSize: "40px",
-        lineHeight: "53px",
+        lineHeight: "47px",
         letterSpacing: "0.40px",
     },
     45: {
         fontSize: "45px",
-        lineHeight: "60px",
+        lineHeight: "53px",
         letterSpacing: "0.45px",
     },
     50: {
         fontSize: "50px",
-        lineHeight: "67px",
+        lineHeight: "58px",
         letterSpacing: "0.50px",
     },
     60: {
         fontSize: "60px",
-        lineHeight: "80px",
+        lineHeight: "71px",
         letterSpacing: "0.60px",
     },
 };
