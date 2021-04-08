5c0a90267df2ce555b13faceae33c3b2eb624efd
Rishabh Mishra <me@rishabhmishra.dev>
2021-04-08 12:16:01 +0530

    refactor(title)!: rename icon to iconProps

--- packages/apsara-ui/src/Title/Title.tsx
+++ packages/apsara-ui/src/Title/Title.tsx
@@ -2,34 +2,31 @@ import React from "react";
 import Tooltip from "../Tooltip";
 import Text from "../Text";
 import "./style.less";
-import Icon, { IconName } from "../Icon/Icon";
+import Icon, { CustomIconProps } from "../Icon/Icon";
 
 interface TitleProps {
     title: string;
-    icon?: {
-        name: IconName;
-        color: string;
-    };
+    iconProps?: CustomIconProps;
     styleOverride?: Record<string, unknown>;
     iconToolTipText?: string;
     status?: string;
 }
 
-const Title = ({ title, icon, styleOverride = {}, iconToolTipText = "", status = "" }: TitleProps) => {
+const Title = ({ title, iconProps, styleOverride = {}, iconToolTipText = "", status = "" }: TitleProps) => {
     return (
         <div className="skeleton-title" style={styleOverride}>
             <Text size={18}> {title} </Text>
-            {icon ? (
+            {iconProps ? (
                 iconToolTipText ? (
                     <Tooltip title={iconToolTipText} placement="right">
-                        <Icon name={icon.name} styleOverride={{ color: icon.color }} />
+                        <Icon {...iconProps} />
                     </Tooltip>
                 ) : (
-                    <Icon name={icon.name} styleOverride={{ color: icon.color }} />
+                    <Icon {...iconProps} />
                 )
             ) : null}
             <Text size={12} className="status_info">
-                {status}{" "}
+                {status}
             </Text>
         </div>
     );
