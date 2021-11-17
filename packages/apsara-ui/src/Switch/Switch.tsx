86a5516280b050ab974d7cdfa7429a8c49ba3413
Ankit Tewari <ankt271192@users.noreply.github.com>
2021-11-17 12:47:07 +0530

    chore: update Slider and Switch

--- packages/apsara-ui/src/Switch/Switch.tsx
+++ packages/apsara-ui/src/Switch/Switch.tsx
@@ -1,3 +1,19 @@
-import { Switch } from "antd";
+import styled from "styled-components";
+import { Switch as AntdSwitch } from "antd";
+
+const Switch = styled(AntdSwitch)`
+    &.ant-switch-checked {
+        background-color: ${({ theme }) => theme?.colors?.primary[4]};
+        .ant-switch-loading-icon {
+            color: ${({ theme }) => theme?.colors?.primary[4]};
+        }
+    }
+    .ant-switch-inner {
+        color: ${({ theme }) => theme?.colors?.black[0]};
+    }
+    .ant-switch-handle::before {
+        background-color: ${({ theme }) => theme?.colors?.black[0]};
+    }
+`;
 
 export default Switch;
