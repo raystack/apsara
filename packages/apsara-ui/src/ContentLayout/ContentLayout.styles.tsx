5e1836ee742a447caa1ea4a5bb24870d9626857f
Ankit Tewari <ankt271192@users.noreply.github.com>
2021-12-17 01:05:11 +0530

    chore: remove less support from build

--- packages/apsara-ui/src/ContentLayout/ContentLayout.styles.tsx
+++ packages/apsara-ui/src/ContentLayout/ContentLayout.styles.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Layout as AntdLayout } from "antd";
 
-export const Layout = styled(AntdLayout)`
+export const Layout: typeof AntdLayout = styled(AntdLayout)`
     background: transparent;
 `;
