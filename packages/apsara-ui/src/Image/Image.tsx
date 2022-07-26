fb42496bc00e63670056399e197a41f3e0470c48
akula-koteswarudu <koteswaruduakula@gmail.com>
2022-07-26 16:39:47 +0530

    feat: added a simple preview option to Image

--- packages/apsara-ui/src/Image/Image.tsx
+++ packages/apsara-ui/src/Image/Image.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 interface ImageProps {
     src: string;
     width?: number;
     height?: number;
+    preview?:  boolean
 }
+
+const Overlay = styled('div')`
+    position: fixed;
+    display: none;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 2;
+    cursor: pointer;
+`
 
 function Image({
     src,
     width,
     height,
+    preview
 }: ImageProps) {
+    const [show, setShow] = useState(false);
     if (!src) return null;
-    return <img src={src} width={width} height={height}/>;
+    return (
+    <>
+        <Overlay onClick={()=>setShow(false)} style={{display:show?"flex":"none"}}>
+            {show && <img  src={src} width="80%" />}
+        </Overlay>
+        <img onClick={preview? ()=>setShow(true): () => null } src={src} width={width} height={height} 
+            style={{cursor: preview? "pointer": "auto"}}/>
+    </>
+    );
 }
 
 export default Image;
