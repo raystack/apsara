75acd04f86f83241040084701b3ec90bfd2eb4fd
akula-koteswarudu <koteswaruduakula@gmail.com>
2022-08-29 00:24:04 +0530

    feat: replaced antd popover with radix popover component

--- packages/apsara-ui/src/Popover/Popover.styles.tsx
+++ packages/apsara-ui/src/Popover/Popover.styles.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+// import React from "react";
+import { violet } from "@radix-ui/colors";
+// import { MixerHorizontalIcon, Cross2Icon } from "@radix-ui/react-icons";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+// import React from "react";
 import styled from "styled-components";
-import { Popover, PopoverProps } from "antd";
 
 export const Container = styled.div`
     font-size: 12px;
@@ -27,30 +30,112 @@ export const Message = styled.div`
 export const Footer = styled.div`
     padding: 12px 16px;
     border-top: 1px solid ${({ theme }) => theme?.colors?.black[4]};
-
     button {
         font-size: 12px;
     }
-
     button + button {
         margin-left: 16px;
     }
 `;
 
-export const StyledPopover = styled(({ className, ...props }: { className?: string } & PopoverProps) => (
-    <Popover {...props} overlayClassName={className} />
-))`
-    .ant-popover-inner {
-        border-radius: 4px;
-        overflow: hidden;
-        background: ${({ theme }) => theme?.popover?.bg};
-    }
+export const StyledContent = styled(PopoverPrimitive.Content)`
+    border-radius: 4px;
+    padding: 20px;
+    width: max-content;
+    background-color: white;
+    box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+    @media (prefers-reduced-motion: no-preference) {
+        animation-duration: 400ms;
+        animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+        will-change: transform opacity;
+        &[data-state="open"] {
+            &[data-side="top"] {
+                animation-name: slideDownAndFade;
+            }
+            &[data-side="right"] {
+                animation-name: slideLeftAndFade;
+            }
+            &[data-side="bottom"] {
+                animation-name: slideUpAndFade;
+            }
+            &[data-side="left"] {
+                animation-name: slideRightAndFade;
+            }
 
-    .ant-popover-inner-content {
-        color: ${({ theme }) => theme?.popover?.text};
-    }
+            @keyframes slideDownAndFade {
+                0% {
+                    opacity: 0;
+                    transform: translateY(-2px);
+                }
+                100% {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
 
-    .ant-popover-arrow-content {
-        background: ${({ theme }) => theme?.popover?.bg};
+            @keyframes slideLeftAndFade {
+                0% {
+                    opacity: 0;
+                    transform: translateX(2px);
+                }
+                100% {
+                    opacity: 1;
+                    transform: translateX(0);
+                }
+            }
+
+            @keyframes slideUpAndFade {
+                0% {
+                    opacity: 0;
+                    transform: translateY(2px);
+                }
+                100% {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+
+            @keyframes slideRightAndFade {
+                0% {
+                    opacity: 0;
+                    transform: translateX(-2px);
+                }
+                100% {
+                    opacity: 1;
+                    transform: translateX(0);
+                }
+            }
+        }
     }
 `;
+
+export const StyledArrow = styled(PopoverPrimitive.Arrow)`
+    fill: white;
+`;
+
+const StyledClose = styled(PopoverPrimitive.Close)`
+    all: unset;
+    font-family: inherit;
+    border-radius: 100%;
+    height: 25px;
+    width: 25px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    color: ${violet.violet11};
+    position: absolute;
+    top: 5px;
+    right: 5px;
+
+    &:hover {
+        background-color: ${violet.violet4};
+    }
+    &:focus {
+        box-shadow: 0 0 0 2px ${violet.violet7};
+    }
+`;
+
+// Exports
+export const StyledPopover = PopoverPrimitive.Root;
+export const PopoverTrigger = PopoverPrimitive.Trigger;
+export const PopoverClose = StyledClose;
