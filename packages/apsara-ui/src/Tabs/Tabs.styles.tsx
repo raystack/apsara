902a024c658afa10c52fa3e80050cf1ead4ea9f6
akula-koteswarudu <koteswaruduakula@gmail.com>
2022-08-29 00:29:11 +0530

    feat: replaced antd tabs component with radix tabs

--- packages/apsara-ui/src/Tabs/Tabs.styles.tsx
+++ packages/apsara-ui/src/Tabs/Tabs.styles.tsx
@@ -1,7 +1,170 @@
-import styled from "styled-components";
-import { Tabs } from "antd";
+import styled, { css } from "styled-components";
+// import { Tabs } from "antd";
+// import React from "react";
+// import { styled } from '@stitches/react';
+import { mauve } from "@radix-ui/colors";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-export const StyledTabs = styled(Tabs)`
+const StyledTabs = styled(TabsPrimitive.Root)`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const StyledList = styled(TabsPrimitive.List)<{ size: "xs" | "md" | "lg" }>`
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    border-bottom: 1px solid ${mauve.mauve6};
+    ${({ size }) =>
+        size === "xs"
+            ? css`
+                  gap: 20px;
+              `
+            : size === "md"
+            ? css`
+                  gap: 34px;
+              `
+            : css`
+                  gap: 82px;
+              `}
+
+    & .right {
+        margin-left: auto;
+        margin-bottom: 5px;
+    }
+
+    & .left {
+        margin-bottom: 5px;
+    }
+`;
+
+const StyledTrigger = styled(TabsPrimitive.Trigger)<{
+    typ: "primary" | "secondary" | "thin";
+}>`
+    all: unset;
+    font-family: inherit;
+    background-color: white;
+    padding: 10px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    line-height: 1em;
+    transition: box-shadow 0.6s;
+    user-select: none;
+    &:first-child {
+        border-top-left-radius: 6px;
+    }
+    &:last-child {
+        border-top-right-radius: 6px;
+    }
+    &[data-state="active"] {
+        box-shadow: inset 0 -1px 0 0 currentColor, 0 1px 0 0 currentColor;
+    }
+    &:focus {
+        position: relative;
+    }
+    ${({ typ, theme }) =>
+        typ === "primary"
+            ? css`
+                  color: ${theme?.colors?.primary[4]};
+                  font-weight: 500;
+                  letter-spacing: 0.35px;
+                  &[data-state="active"] {
+                      color: ${theme?.colors?.primary[6]};
+                  }
+                  &:hover {
+                      color: ${theme?.colors?.primary[5]};
+                  }
+                  &:focus {
+                      color: ${theme?.colors?.primary[6]};
+                  }
+
+                  &[disabled] {
+                      color: ${theme?.colors?.black[5]};
+                      cursor: not-allowed;
+                      &:hover,
+                      &:focus,
+                      &:active {
+                          color: ${theme?.colors?.black[5]};
+                      }
+                  }
+              `
+            : typ === "secondary"
+            ? css`
+                  color: ${theme?.colors?.black[7]};
+                  font-size: 16px;
+                  font-weight: 500;
+                  letter-spacing: 0.4px;
+                  &[data-state="active"] {
+                      color: ${theme?.colors?.black[9]};
+                  }
+                  &:hover {
+                      color: ${theme?.colors?.black[8]};
+                  }
+                  &:focus {
+                      color: ${theme?.colors?.black[9]};
+                  }
+
+                  &[disabled] {
+                      color: ${theme?.colors?.black[4]};
+                      cursor: not-allowed;
+                      &:hover,
+                      &:focus,
+                      &:active {
+                          color: ${theme?.colors?.black[4]};
+                      }
+                  }
+              `
+            : typ == "thin"
+            ? css`
+                  color: ${theme?.colors?.black[7]};
+                  font-size: 12px;
+                  font-weight: bold;
+                  letter-spacing: 0.4px;
+                  &[data-state="active"] {
+                      color: ${theme?.colors?.black[9]};
+                  }
+                  &:hover {
+                      color: ${theme?.colors?.black[8]};
+                  }
+                  &:focus {
+                      color: ${theme?.colors?.black[9]};
+                  }
+
+                  &[disabled] {
+                      color: ${theme?.colors?.black[4]};
+                      cursor: not-allowed;
+                      &:hover,
+                      &:focus,
+                      &:active {
+                          color: ${theme?.colors?.black[4]};
+                      }
+                  }
+              `
+            : null}
+`;
+
+const StyledContent = styled(TabsPrimitive.Content)`
+    flex-grow: 1;
+    padding: 10px;
+    padding-top: 20px;
+    background-color: white;
+    border-bottom-left-radius: 6px;
+    border-bottom-right-radius: 6px;
+    outline: none;
+`;
+
+// Exports
+export const Tabs = StyledTabs;
+export const TabsList = StyledList;
+export const TabsTrigger = StyledTrigger;
+export const TabsContent = StyledContent;
+
+export const Styledabs = styled(Tabs)`
     .ant-tabs-ink-bar {
         bottom: 0px;
     }
