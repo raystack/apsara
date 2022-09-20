e5719aae596732a3d3048b7e9d50c53eb99ea448
Abhishek Verma <abhishek.v.skuad@aux.gojek.com>
2022-09-20 13:16:25 +0530

    remove unused import

--- packages/apsara-ui/src/Segments/Segments.styles.tsx
+++ packages/apsara-ui/src/Segments/Segments.styles.tsx
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { css } from "styled-components";
 import { capitalize } from "../mixin";
 import { StyledTag } from "../Tag/Tag.styles";
 
