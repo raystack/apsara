e37bfa67211e9bb7a4208ade65aa505ba493510d
Abhishek Verma <abhishek.v.skuad@aux.gojek.com>
2022-09-27 15:53:23 +0530

    initial skeleton implementation

---
+++ packages/apsara-ui/src/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ListSkeleton, EditorSkeleton, DetailsSkeleton } from "./Skeleton";
+
+export default {
+    title: "Feedback/Skeleton",
+    component: ListSkeleton,
+};
+export const listloader = () => <ListSkeleton />;
+export const editorLoader = () => <EditorSkeleton lastLineWidth="80%" />
+export const detailsloader = () => <DetailsSkeleton />
