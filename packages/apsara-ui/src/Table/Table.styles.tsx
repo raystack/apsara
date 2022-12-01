440f4cccf4a7689c1c2d348e044c0a7c6100f7d7
Abhishek Verma <abhishek.v.skuad@aux.gojek.com>
2022-12-01 17:26:30 +0530

    fix ui issue with button having href in table

--- packages/apsara-ui/src/Table/Table.styles.tsx
+++ packages/apsara-ui/src/Table/Table.styles.tsx
@@ -58,7 +58,7 @@ export const StyledTable = styled(Table)`
         .virtual-table-cell {
             cursor: pointer;
 
-            a {
+            > a {
                 display: block;
                 color: inherit;
                 white-space: nowrap;
