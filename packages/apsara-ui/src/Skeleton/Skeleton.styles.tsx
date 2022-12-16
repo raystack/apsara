cfa219cce160cea673cb123977045aaa61988597
Abhishek Verma <abhishek.v.skuad@aux.gojek.com>
2022-12-16 14:36:24 +0530

    fix: skeleton animation after removing loader component

--- packages/apsara-ui/src/Skeleton/Skeleton.styles.tsx
+++ packages/apsara-ui/src/Skeleton/Skeleton.styles.tsx
@@ -1,4 +1,13 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const skeletonClsLoading = keyframes`
+    0% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0 50%;
+    }
+`;
 
 export const StyledSkeleton = styled.div<{ $props?: any; $editor?: boolean }>`
 height:24px;
@@ -17,9 +26,9 @@ ul.skeleton-paragraph{
 `;
 
 export const ListItem = styled.li`
-    background: linear-gradient(90deg, rgb(245, 245, 245) 25%, rgb(234, 234, 234) 37%, rgb(245, 245, 245) 63%) 0% 0% /
-        400% 100%;
-    animation: ant-skeleton-loading 1.4s ease infinite;
+    background: linear-gradient(90deg, rgb(245, 245, 245) 25%, rgb(234, 234, 234) 37%, rgb(245, 245, 245) 63%) 0% 0% /400%
+        100%;
+    animation: ${skeletonClsLoading} 1.4s ease infinite;
     height: 24px;
     list-style: none;
     border-radius: 4px;
@@ -28,13 +37,12 @@ export const ListItem = styled.li`
 export const StyledRow = styled.div<{ $props?: any }>`
     background: linear-gradient(90deg, rgb(245, 245, 245) 25%, rgb(234, 234, 234) 37%, rgb(245, 245, 245) 63%) 0% 0% /
         400% 100%;
-    animation: ant-skeleton-loading 1.4s ease infinite;
+    animation: ${skeletonClsLoading} 1.4s ease infinite;
     height: ${({ $props }) => $props.height};
     border-radius: 4px;
     max-width: ${({ $props }) => calculateWidth($props.maxWidth)};
     flex: ${({ $props }) => `0 0 ${calculateWidth($props.width)}}`};
 `;
-
 const calculateWidth = (width: number) => {
     return (width / 24) * 100 + "%";
 };
