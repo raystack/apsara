4b8ac157d284a937d316b2aeff27784abc1e3da7
Praveen Yadav <pyadav9678@gmail.com>
2023-03-23 15:19:17 +0530

    feat: create a base setup with
    1. Rollup to build
    2. Typescript
    3. stitches for css-in-js
    4. NextJS for wabsite
    5. Redix for stateless components

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import { getCssText, reset } from "~/stitches.config";
+
+/**
+ * Get the css and reset the internal css representation.
+ * This is very *IMPORTANT* to do as the server might handle multiple requests
+ * and we don't want to have the css accumulated from previous requests
+ */
+const getCssAndReset = () => {
+    const css = getCssText();
+    reset();
+    return css;
+};
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
