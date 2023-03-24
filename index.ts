a048cd97d97c264353c960d6e30db28a4587a1ea
Praveen Yadav <pyadav9678@gmail.com>
2023-03-24 16:21:57 +0530

    feat: export stitches functions as part of library

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
+export { config, createTheme, css, getCssText, globalCss, keyframes, reset, styled, theme } from "~/stitches.config";
+// components
 export { Box } from "./components/box";
+export { ApsaraGlobalStyle } from "./components/global";
--- stitches.config.ts
+++ stitches.config.ts
@@ -470,41 +470,6 @@ export const dark = createTheme("dark", {
     },
 });
 
-export const globalStyles = globalCss({
-    "*, *:before, *:after": {
-        margin: 0,
-        boxSizing: "border-box",
-    },
-    "*": {
-        margin: 0,
-    },
-    html: {
-        fontSize: "10px",
-    },
-    "html, body": {
-        fontFamily: "$inter",
-    },
-    body: {
-        fontSize: "1.2rem",
-        color: "$hiContrast",
-        backgroundColor: "$olive1",
-        overflowX: "hidden",
-        textRendering: "optimizelegibility",
-        "-webkit-font-smoothing": "antialiased",
-    },
-    a: {
-        color: "$anchor",
-    },
-
-    [`${dark}`]: {},
-});
-
-globalStyles();
-globalCss({
-    "@font-face": [],
-    "@import": ["url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap')"],
-})();
-
 type CSS = Stitches.CSS<typeof config>;
 type StitchesConfig = typeof config;
 type ScaleValue<TValue> = Stitches.ScaleValue<TValue, StitchesConfig>;
