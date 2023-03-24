88bffb8aa75210aa39a2de469004a012bc7ad4de
Praveen Yadav <pyadav9678@gmail.com>
2023-03-24 16:21:57 +0530

    feat: export stitches functions as part of library

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import commonjs from "rollup-plugin-commonjs";
-import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
