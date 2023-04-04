c63a43e8c498ae5d7a472536bc666e70cf525ac9
Praveen Yadav <pyadav9678@gmail.com>
2023-04-04 11:37:33 +0530

    fix: rollup tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -42,7 +42,6 @@ export default {
         commonjs(),
         typescript({
             clean: true,
-            tsconfig: "rollup-tsconfig.json",
             typescript: require("typescript"),
         }),
     ],
