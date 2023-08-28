e9f1b14f1bdde3fd25bb1356af9d5d3796ca57c4
Praveen Yadav <pyadav9678@gmail.com>
2023-08-28 21:32:31 +0530

    feat: add error state along wiht css fix

--- packages/raystack/table/DataTableGloabalSearch.tsx
+++ packages/raystack/table/DataTableGloabalSearch.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { ComponentProps } from "react";
 import { TextField } from "~/textfield";
 import { useTable } from "./hooks/useTable";
@@ -8,6 +9,8 @@ export function DataTableGloabalSearch(props: DataTableGloabalSearchProps) {
 
   return (
     <TextField
+      // @ts-ignore
+      leading={<MagnifyingGlassIcon />}
       placeholder="Filter tasks..."
       value={globalFilter ?? ""}
       onChange={(event) => onGlobalFilterChange(String(event.target.value))}
