f835c6289f301744eff26ce5cc289d3db3e01bc9
Rishabh <me@rsbh.dev>
2024-02-19 12:37:47 +0530

    feat: add theme switcher to navbar

--- apps/www/components/Navbar/links.tsx
+++ apps/www/components/Navbar/links.tsx
@@ -1,10 +1,11 @@
-import { Flex, Link } from "@raystack/apsara";
+import { Flex, Link, ThemeSwitcher } from "@raystack/apsara";
 
 export const NavbarLinks = () => {
   return (
     <Flex gap="large">
       <Link href="/docs/primitives/overview/introduction">Doccumentation</Link>
       <Link href="/examples">Examples</Link>
+      <ThemeSwitcher size={16} />
     </Flex>
   );
 };
