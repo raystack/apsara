9ac4ab24dd8fc91d7a0c61455d7f61a80de93d09
Rishabh <me@rsbh.dev>
2024-05-21 11:50:31 +0530

    add data column in example

--- examples/shield-ts/src/assets.tsx
+++ examples/shield-ts/src/assets.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   useTable,
 } from "@raystack/apsara";
+import dayjs from "dayjs";
 import * as React from "react";
 
 const data: Payment[] = [
@@ -14,30 +15,35 @@ const data: Payment[] = [
     amount: 316,
     status: "success",
     email: "ken99@yahoo.com",
+    created_at: "2023-10-10T13:02:56.12Z",
   },
   {
     id: "3u1reuv4",
     amount: 242,
     status: "success",
     email: "Abe45@gmail.com",
+    created_at: "2023-09-15T11:55:46.28Z",
   },
   {
     id: "derv1ws0",
     amount: 837,
     status: "processing",
     email: "Monserrat44@gmail.com",
+    created_at: "2023-07-01T10:02:50.60Z",
   },
   {
     id: "5kma53ae",
     amount: 874,
     status: "success",
     email: "Silas22@gmail.com",
+    created_at: "2023-05-30T09:02:26.49Z",
   },
   {
     id: "bhqecj4p",
     amount: 721,
     status: "failed",
     email: "carmella@hotmail.com",
+    created_at: "2024-04-20T21:02:01.25Z",
   },
 ];
 
@@ -46,6 +52,7 @@ export type Payment = {
   amount: number;
   status: "pending" | "processing" | "success" | "failed";
   email: string;
+  created_at: string;
 };
 
 export const columns: ApsaraColumnDef<Payment>[] = [
@@ -91,7 +98,7 @@ export const columns: ApsaraColumnDef<Payment>[] = [
   {
     accessorKey: "email",
     header: "Email",
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+    cell: ({ row, getValue }) => <div className="lowercase">{getValue()}</div>,
     filterVariant: "text",
   },
   {
@@ -108,6 +115,16 @@ export const columns: ApsaraColumnDef<Payment>[] = [
       return <div className="text-right font-medium">{formatted}</div>;
     },
     filterVariant: "number",
+  },
+  {
+    accessorKey: "created_at",
+    header: "Created at",
+    cell: ({ row, getValue }) => (
+      <div className="lowercase">
+        {dayjs(getValue()).format("DD MMM YY HH:mm")}
+      </div>
+    ),
+    filterVariant: "text",
   },
 ];
 
