d966e83ec3bf4877c2c93ed67bec0e688f33cec6
Rishabh <me@rsbh.dev>
2024-05-23 09:55:44 +0530

    feat: add range picker component

--- packages/raystack/calendar/index.tsx
+++ packages/raystack/calendar/index.tsx
@@ -1,2 +1,3 @@
 export { Calendar } from "./calendar";
 export { DatePicker } from "./date-picker";
+export { RangePicker } from "./range-picker";
---
+++ packages/raystack/calendar/range-picker.tsx
@@ -0,0 +1,80 @@
+import { Popover } from "~/popover";
+import { TextField } from "~/textfield";
+import { Calendar, CalendarProps } from "./calendar";
+import styles from "./calendar.module.css";
+import { useState } from "react";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import dayjs from "dayjs";
+import { TextfieldProps } from "~/textfield/textfield";
+import { DateRange, SelectRangeEventHandler } from "react-day-picker";
+import { Flex } from "~/flex";
+
+interface RangePickerProps {
+  side?: "top" | "right" | "bottom" | "left";
+  dateFormat?: string;
+  textFieldProps?: TextfieldProps;
+  calendarProps?: CalendarProps;
+  onSelect?: (date: DateRange) => void;
+  pickerGroupClassName?: string;
+}
+
+export function RangePicker({
+  side = "top",
+  dateFormat = "DD/MM/YYYY",
+  textFieldProps,
+  calendarProps,
+  onSelect = () => {},
+  pickerGroupClassName,
+}: RangePickerProps) {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [value, setValue] = useState<DateRange>({
+    to: new Date(),
+    from: new Date(),
+  });
+  const startDate = dayjs(value.from).format(dateFormat);
+  const endDate = dayjs(value.to).format(dateFormat);
+
+  const handleSelect: SelectRangeEventHandler = (range, selectedDay) => {
+    if (range) {
+      setValue(range);
+    }
+    if (range?.to === selectedDay) {
+      onOpenChange(false);
+      onSelect(range);
+    }
+  };
+
+  function onOpenChange(open?: boolean) {
+    setShowCalendar(Boolean(open));
+  }
+
+  return (
+    <Popover open={showCalendar} onOpenChange={onOpenChange}>
+      <Popover.Trigger asChild>
+        <Flex gap={"medium"} className={pickerGroupClassName}>
+          <TextField
+            value={startDate}
+            tailing={<CalendarIcon />}
+            className={styles.datePickerInput}
+            {...textFieldProps}
+          />
+          <TextField
+            value={endDate}
+            tailing={<CalendarIcon />}
+            className={styles.datePickerInput}
+            {...textFieldProps}
+          />
+        </Flex>
+      </Popover.Trigger>
+      <Popover.Content side={side} className={styles.calendarPopover}>
+        <Calendar
+          numberOfMonths={2}
+          {...calendarProps}
+          mode="range"
+          selected={value}
+          onSelect={handleSelect}
+        />
+      </Popover.Content>
+    </Popover>
+  );
+}
