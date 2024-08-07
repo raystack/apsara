fc61fb75b67e255a6df255452561977190182567
singh-pk <pksn1111@gmail.com>
2024-08-07 14:19:29 +0530

    fix: DatePicker getting closed when Calendar dropdown is open and clicked outside of the dropdown

--- packages/raystack/calendar/calendar.tsx
+++ packages/raystack/calendar/calendar.tsx
@@ -8,21 +8,29 @@ import {
 } from "@radix-ui/react-icons";
 import styles from "./calendar.module.css";
 import { Select } from "~/select";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Flex } from "~/flex/flex";
 
-export type CalendarProps = DayPickerProps & {};
+export type onDropdownOpen = () => {};
+export type CalendarProps = DayPickerProps & onDropdownOpen;
 
 const root = cva(styles.calendarRoot);
 
-function DropDown({ options = [], value, onChange }: DropdownProps) {
+function DropDown({ options = [], value, onChange, onDropdownOpen }: (DropdownProps & onDropdownOpen)) {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) onDropdownOpen();
+  }, [open]);
+
   function handleChange(value: string) {
     if (onChange) {
       onChange({ target: { value } } as ChangeEvent<HTMLSelectElement>);
     }
   }
+
   return (
-    <Select value={value?.toString()} onValueChange={handleChange}>
+    <Select value={value?.toString()} onValueChange={handleChange} open={open} onOpenChange={setOpen}>
       <Select.Trigger
         className={styles.dropdown_trigger}
         iconProps={{
@@ -65,6 +73,7 @@ export const Calendar = function ({
   className,
   classNames,
   showOutsideDays = true,
+  onDropdownOpen,
   ...props
 }: CalendarProps) {
   return (
@@ -77,7 +86,7 @@ export const Calendar = function ({
           }
           return <ChevronRightIcon {...props} />;
         },
-        Dropdown: DropDown,
+        Dropdown: (props: DropdownProps) => <DropDown {...props} onDropdownOpen={onDropdownOpen} />,
       }}
       classNames={{
         caption_label: styles.caption_label,
