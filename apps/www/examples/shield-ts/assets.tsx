74b91768e0439da7630a06336820ced0cff1caa2
paanSinghCoder <gaurav3017@gmail.com>
2024-08-29 12:34:40 +0530

    chore: remove ununsed import

--- apps/www/examples/shield-ts/assets.tsx
+++ apps/www/examples/shield-ts/assets.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import dayjs from "dayjs";
 import {
   ApsaraColumnDef,
