55eca4bcc87de30e61d808c198192dc709b781e0
singh-pk <pksn1111@gmail.com>
2024-09-12 18:02:56 +0530

    feat: TextField editable in date-picker

--- packages/raystack/calendar/date-picker.tsx
+++ packages/raystack/calendar/date-picker.tsx
@@ -2,7 +2,7 @@ import { Popover } from "~/popover";
 import { TextField } from "~/textfield";
 import { Calendar } from "./calendar";
 import styles from "./calendar.module.css";
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
 import dayjs from "dayjs";
 import { TextfieldProps } from "~/textfield/textfield";
@@ -25,14 +25,51 @@ export function DatePicker({
   value = new Date(),
   onSelect = () => {},
 }: DatePickerProps) {
+  const defaultDate = dayjs(value).format(dateFormat);
+
   const [showCalendar, setShowCalendar] = useState(false);
-  const dateValue = dayjs(value).format(dateFormat);
+  const [calendarVal, setCalendarVal] = useState(value);
+  const [inputState, setInputState] = useState<Partial<React.ComponentProps<typeof TextField>['state']>>();
 
   const isDropdownOpenedRef = useRef(false);
+  const textFieldRef = useRef<HTMLInputElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const isInputFieldFocused = useRef(false);
+
+  const handleMouseUp = useCallback((event: MouseEvent) => {
+      const el = (event.target) as HTMLElement | null;
+
+      if (!isDropdownOpenedRef.current && // Month and Year dropdown from Date picker
+          !textFieldRef.current?.contains(el) && // TextField
+          !contentRef.current?.contains(el)) { // DatePicker Popper
+              removeEventListeners();
+         }
+  }, [])
+
+  function registerEventListeners() {
+    isInputFieldFocused.current = true;
+    document.addEventListener('mouseup', handleMouseUp);
+  }
+
+  function removeEventListeners() {
+    isInputFieldFocused.current = false;
+    setShowCalendar(false);
+    if (inputState === undefined) onSelect(calendarVal);
+    document.removeEventListener('mouseup', handleMouseUp);
+  }
 
   const handleSelect = (day: Date) => {
     onSelect(day);
+    setCalendarVal(day);
+
+    setInputState(undefined);
+
+    if  (textFieldRef.current) {
+        textFieldRef.current.value = dayjs(day).format(dateFormat);
+    }
+
     setShowCalendar(false);
+    removeEventListeners();
   };
 
   function onDropdownOpen() {
@@ -40,6 +77,8 @@ export function DatePicker({
   }
 
   function onOpenChange(open?: boolean) {
+    if (isInputFieldFocused.current && showCalendar) return;
+
     if (!isDropdownOpenedRef.current) {
       setShowCalendar(Boolean(open));
     }
@@ -47,26 +86,62 @@ export function DatePicker({
     isDropdownOpenedRef.current = false;
   }
 
+  function handleInputFocus() {
+    if (isInputFieldFocused.current) return;
+    if (!showCalendar) setShowCalendar(true);
+  }
+
+  function handleInputBlur() {
+    if (isInputFieldFocused.current) {
+        removeEventListeners();
+    } else {
+        registerEventListeners();
+        setTimeout(() => textFieldRef.current?.focus());
+    }
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = (event.target);
+    const date = dayjs(value);
+
+    const isValidDate = date.isValid();
+
+    const isAfter = calendarProps?.startMonth !== undefined ? dayjs(date).isSameOrAfter(calendarProps.startMonth) : true;
+    const isBefore = calendarProps?.endMonth !== undefined ? dayjs(date).isSameOrBefore(calendarProps.endMonth) : true;
+
+    const isValid = isValidDate && isAfter && isBefore && dayjs(date).isSameOrBefore(dayjs());
+
+    if (isValid) {
+        setCalendarVal(date.toDate());
+        if (inputState === 'invalid') setInputState(undefined);
+    } else {
+        setInputState('invalid');
+    }
+  }
+
   return (
     <Popover open={showCalendar} onOpenChange={onOpenChange}>
-      <Popover.Trigger asChild>
-        <TextField
-          value={dateValue}
-          trailing={<CalendarIcon />}
-          className={styles.datePickerInput}
-          readOnly
+      <TextField
+          ref={textFieldRef}
+          defaultValue={defaultDate}
+          trailing={<Popover.Trigger asChild><CalendarIcon /></Popover.Trigger>}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          state={inputState}
           {...textFieldProps}
-        />
-      </Popover.Trigger>
-      <Popover.Content side={side} className={styles.calendarPopover}>
+      />
+
+      <Popover.Content side={side} className={styles.calendarPopover} ref={contentRef}>
         <Calendar
           required={true}
           {...calendarProps}
           onDropdownOpen={onDropdownOpen}
           mode="single"
-          selected={value}
-          defaultMonth={value}
+          selected={calendarVal}
+          month={calendarVal}
           onSelect={handleSelect}
+          onMonthChange={setCalendarVal}
         />
       </Popover.Content>
     </Popover>
