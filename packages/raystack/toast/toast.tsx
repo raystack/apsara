d66617dabf0bcd6f4920a201c56fa6a5d8e49930
paanSinghCoder <gaurav3017@gmail.com>
2024-11-12 18:33:31 +0530

    docs: depricate old components

--- packages/raystack/toast/toast.tsx
+++ packages/raystack/toast/toast.tsx
@@ -2,10 +2,16 @@ import { Toaster, type ToasterProps, toast as sonnerToast } from "sonner";
 
 interface ToastContainerProps extends ToasterProps {}
 
+/**
+ * @deprecated Use ToastContainer from '@raystack/apsara/v1' instead.
+ */
 function ToastContainer(props: ToastContainerProps) {
   return <Toaster {...props} />;
 }
 
+/**
+ * @deprecated Use toast from '@raystack/apsara/v1' instead.
+ */
 const toast: typeof sonnerToast = Object.assign(
   (...props: Parameters<typeof sonnerToast>) => sonnerToast(...props),
   sonnerToast
