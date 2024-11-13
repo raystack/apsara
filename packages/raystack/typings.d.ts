30370e0ed3ec003cecbae335507db4a6b31be975
paanSinghCoder <gaurav3017@gmail.com>
2024-11-13 12:20:38 +0530

    fix: merge typing and typings declaration files into one

--- packages/raystack/typings.d.ts
+++ packages/raystack/typings.d.ts
@@ -7,3 +7,16 @@ declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+export declare module "@tanstack/table-core" {
+  export interface ColumnMeta<TData extends RowData, TValue> {
+    style?: Record<string, string>;
+  }
+}
+
+export type TableColumnMetadata = {
+  name: ReactNode | Element;
+  key: string;
+  value: string;
+  label?: string;
+};
