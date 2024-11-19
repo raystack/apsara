15215867a8f63a8f938cd8a4e99326d920a2ea98
Rishabh <me@rsbh.dev>
2024-11-19 16:14:28 +0530

    feat: add empty state component

---
+++ packages/raystack/v1/components/emptystate/emptystate.tsx
@@ -0,0 +1,70 @@
+import { cva } from "class-variance-authority";
+import styles from "./emptystate.module.css";
+const emptystate = cva(styles.emptystate);
+import { Flex } from "../flex";
+import { Text } from "../text";
+import clsx from "clsx";
+
+type classNameKeys =
+  | "container"
+  | "iconContainer"
+  | "icon"
+  | "heading"
+  | "subHeading";
+
+interface EmptystateProps {
+  icon: React.ReactNode;
+  heading?: React.ReactNode;
+  subHeading?: React.ReactNode;
+  primaryAction?: React.ReactNode;
+  secondaryAction?: React.ReactNode;
+  classNames?: Partial<Record<classNameKeys, string>>;
+}
+
+export const EmptyState = ({
+  icon,
+  heading,
+  subHeading,
+  primaryAction,
+  secondaryAction,
+  classNames,
+}: EmptystateProps) => {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      gap="medium"
+      className={clsx(styles.emptyState, classNames?.container)}
+    >
+      <div className={clsx(styles.iconContainer, classNames?.iconContainer)}>
+        <div className={clsx(styles.icon, classNames?.icon)}>{icon}</div>
+      </div>
+
+      <Flex direction="column" gap="small" align="center">
+        {heading && (
+          <Text
+            size={5}
+            weight={500}
+            className={clsx(styles.headerText, classNames?.heading)}
+          >
+            {heading}
+          </Text>
+        )}
+
+        {subHeading && (
+          <Text
+            size={4}
+            weight={400}
+            className={clsx(styles.subHeaderText, classNames?.subHeading)}
+          >
+            {subHeading}
+          </Text>
+        )}
+      </Flex>
+
+      {primaryAction}
+
+      {secondaryAction}
+    </Flex>
+  );
+};
