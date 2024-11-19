ef45972f5906078c5262c1593066821c934857a6
Rishabh <me@rsbh.dev>
2024-11-19 16:14:47 +0530

    chore: deprecate old empty state component

--- packages/raystack/emptystate/emptystate.tsx
+++ packages/raystack/emptystate/emptystate.tsx
@@ -7,6 +7,9 @@ const emptystate = cva(styles.emptystate);
 type EmptystateProps = PropsWithChildren<VariantProps<typeof emptystate>> &
   HTMLAttributes<HTMLElement>;
 
+/**
+ * @deprecated Use EmptyState from '@raystack/apsara/v1' instead.
+ */
 export function EmptyState({ children, className, ...props }: EmptystateProps) {
   return (
     <div className={emptystate({ className })} {...props}>
