7fea2865c5283eb13860c54c872c65b539621ecc
paanSinghCoder <gaurav3017@gmail.com>
2024-11-22 11:37:17 +0530

    feat: add import sorter

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,12 +1,16 @@
+
 module.exports = {
   extends: ["next", "turbo", "prettier"],
+  plugins: [ "simple-import-sort" ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/display-name": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve("next/babel")]
     },
   },
 };
