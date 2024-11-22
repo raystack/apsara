6925242c39c1a1ceb03b73952f9d075e36f17d9d
Gaurav Singh <gaurav3017@gmail.com>
2024-11-22 12:36:22 +0530

    fix: imports and rename (#188)
    * fix: imports and rename
    
    * chore: temp change name
    
    * chore: rename file

---
+++ packages/raystack/v1/components/themeProvider/index.tsx
@@ -0,0 +1,4 @@
+export { ThemeSwitcher } from "./switcher";
+export { ThemeProvider, useTheme } from "./theme";
+export { ThemeProviderProps } from "./types";
+// Note: This themeProvider folder is a merge of old and the new themeProvider. Both old and the v1 folder contains the exact copy of themeprovider which was merged.
