115361de40e1ecae092b4174eccc5096a5d046b1
paanSinghCoder <gaurav3017@gmail.com>
2024-12-04 17:02:15 +0530

    chore: import sorted

--- packages/raystack/v1/index.tsx
+++ packages/raystack/v1/index.tsx
@@ -5,13 +5,13 @@ export { Box } from "./components/box"
 export { Breadcrumb } from "./components/breadcrumb";
 export { Button } from "./components/button";
 export { Checkbox } from "./components/checkbox";
+export { Chip } from './components/chip'
 export { DropdownMenu } from "./components/dropdownMenu";
 export { EmptyState } from "./components/emptyState";
 export { Flex } from "./components/flex";
 export { InputField } from "./components/inputField";
 export { Spinner } from "./components/spinner";
 export { Text } from "./components/text";
-export { Chip } from './components/chip'
 export {
   ThemeProvider,
   ThemeProviderProps,
