1033b906f83930c2548b576e4107887f819aeada
Gaurav Singh <gaurav3017@gmail.com>
2024-12-09 14:33:44 +0530

    feat: add badge v1 component(#194)
    * feat: add exports
    
    * feat: add badge component
    
    * style: add css
    
    * chore: add newBadge
    
    * chore: add assets usage
    
    * docs: add mdx
    
    * fix: combine common classes
    
    * chore: change default
    
    * docs: add default props to docs
    
    * docs: typo fix
    
    * chore: default prop

--- apps/www/examples/shield-ts/assets.tsx
+++ apps/www/examples/shield-ts/assets.tsx
@@ -7,7 +7,7 @@ import {
   useTable
 } from "@raystack/apsara";
 
-import { toast, ToastContainer, Avatar, AvatarGroup, Button, Spinner, DropdownMenu, Breadcrumb,  Flex, Text, Checkbox, InputField } from "@raystack/apsara/v1";
+import { toast, ToastContainer, Avatar, AvatarGroup, Button, Spinner, DropdownMenu, Breadcrumb,  Flex, Text, Checkbox, InputField, Badge } from "@raystack/apsara/v1";
 import { getData, Payment } from "./data";
 import { ApsaraColumnDef } from "@raystack/apsara/table/datatables.types";
 const TOTAL_PAGES = 100;
@@ -224,6 +224,9 @@ const AssetsHeader = () => {
           placeholder="Place holder"
           prefix="USD"
         />
+        <Badge size="small" variant="gradient" icon={<HomeIcon />}>
+          Custom Badge
+        </Badge>
         {/* <Button variant="outline">Click here</Button>
         <Breadcrumb items={items} size="small" />
         <DropdownMenu>
