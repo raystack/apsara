f738e4b15eae9e9c55aed8ca90bc9e16fa6dc41f
paanSinghCoder <gaurav3017@gmail.com>
2024-12-10 11:20:10 +0530

    feat(switch): add initial switch.tsx

---
+++ packages/raystack/v1/components/switch/switch.tsx
@@ -0,0 +1,46 @@
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+import { cva } from "class-variance-authority";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import styles from "./switch.module.css";
+
+const switchVariants = cva(styles.switch);
+
+export interface SwitchProps
+  extends ComponentPropsWithoutRef<typeof SwitchPrimitive.Root> {
+  disabled?: boolean;
+  required?: boolean;
+}
+
+export const Switch = forwardRef<
+  ElementRef<typeof SwitchPrimitive.Root>,
+  SwitchProps
+>(({ className, disabled, required, ...props }, forwardedRef) => (
+  <SwitchPrimitive.Root
+    {...props}
+    ref={forwardedRef}
+    disabled={disabled}
+    required={required}
+    className={switchVariants({ className })}
+    data-disabled={disabled}
+  >
+    <SwitchThumb />
+  </SwitchPrimitive.Root>
+));
+
+const thumbVariants = cva(styles.thumb);
+
+interface ThumbProps
+  extends ComponentPropsWithoutRef<typeof SwitchPrimitive.Thumb> {}
+
+const SwitchThumb = forwardRef<
+  ElementRef<typeof SwitchPrimitive.Thumb>,
+  ThumbProps
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitive.Thumb
+    ref={ref}
+    className={thumbVariants({ className })}
+    {...props}
+  />
+));
+
+Switch.displayName = "Switch";
