d31e427c640ed1d17df68081e0e559cb947ecd37
paanSinghCoder <gaurav3017@gmail.com>
2024-12-11 12:03:06 +0530

    fix: change props in new radio button

--- packages/raystack/v1/components/radio/radio.tsx
+++ packages/raystack/v1/components/radio/radio.tsx
@@ -3,37 +3,26 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import styles from "./radio.module.css";
 
-const RedioRoot = forwardRef<
+const RadioRoot = forwardRef<
   ElementRef<typeof RadioGroupPrimitive.Root>,
   ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <RadioGroupPrimitive.Root ref={ref} className={styles.radio} {...props} />
 ));
 
-const radioItem = cva(styles.radioitem, {
-  variants: {
-    size: {
-      small: styles["radioitem-small"],
-      medium: styles["radioitem-medium"],
-    },
-  },
-  defaultVariants: {
-    size: "small",
-  },
-});
+const radioItem = cva(styles.radioitem);
 
 export interface RadioItemProps
-  extends ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>,
-    VariantProps<typeof radioItem> {}
+  extends ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> {}
 
 export const RadioItem = forwardRef<
   ElementRef<typeof RadioGroupPrimitive.Item>,
   RadioItemProps
->(({ className, size, ...props }, forwardedRef) => (
+>(({ className, ...props }, forwardedRef) => (
   <RadioGroupPrimitive.Item
     {...props}
     ref={forwardedRef}
-    className={radioItem({ size, className })}
+    className={radioItem({ className })}
   >
     <Indicator />
   </RadioGroupPrimitive.Item>
@@ -57,7 +46,8 @@ const Indicator = forwardRef<
 
 Indicator.displayName = RadioGroupPrimitive.Indicator.displayName;
 
-export const Radio = Object.assign(RedioRoot, {
+export const Radio = Object.assign(RadioRoot, {
+  Root: RadioRoot,
   Indicator: Indicator,
   Item: RadioItem,
 });
