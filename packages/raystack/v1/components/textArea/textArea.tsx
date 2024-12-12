dba2d7fedb11e1d654dd65c3d309718137a63aa2
paanSinghCoder <gaurav3017@gmail.com>
2024-12-12 14:41:30 +0530

    feat: add error, width, value and onChange props

--- packages/raystack/v1/components/textArea/textArea.tsx
+++ packages/raystack/v1/components/textArea/textArea.tsx
@@ -4,7 +4,13 @@ import { VariantProps, cva, cx } from "class-variance-authority";
 
 import styles from "./textarea.module.css";
 
-const textArea = cva(styles.textarea);
+const textArea = cva(styles.textarea, {
+  variants: {
+    error: {
+      true: styles.error
+    }
+  }
+});
 
 export interface TextAreaProps extends PropsWithChildren<VariantProps<typeof textArea>>, 
   HTMLAttributes<HTMLTextAreaElement> {
@@ -12,12 +18,16 @@ export interface TextAreaProps extends PropsWithChildren<VariantProps<typeof tex
   required?: boolean;
   helpIcon?: ReactNode;
   helperText?: string;
+  error?: boolean;
+  width?: string | number;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ className, style, label, required, helpIcon, helperText, ...props }, ref) => {
+  ({ className, style, label, required, helpIcon, helperText, error, width = "200px", value, onChange, ...props }, ref) => {
     return (
-      <div className={styles.container}>
+      <div className={styles.container} style={{ width }}>
         {label && (
           <div className={styles.labelContainer}>
             <label className={styles.label}>
@@ -28,8 +38,10 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
           </div>
         )}
         <textarea
-          className={cx(textArea(), className)}
+          className={cx(textArea({ error }), className)}
           ref={ref}
+          value={value}
+          onChange={onChange}
           {...props}
         />
         {helperText && <span className={styles.helperText}>{helperText}</span>}
