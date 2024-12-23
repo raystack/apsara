301623f4a37e87bf8cf42b5dfc52605e442cb7a2
paanSinghCoder <gaurav3017@gmail.com>
2024-12-23 21:44:03 +0530

    style: update style

--- packages/raystack/v1/components/slider/thumb.tsx
+++ packages/raystack/v1/components/slider/thumb.tsx
@@ -1,5 +1,9 @@
 export const ThumbIcon = () => (
-    <svg width="32" height="28" viewBox="0 0 32 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="32"
+    height="28"
+    viewBox="0 0 32 28"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg">
     <g filter="url(#filter0_dd_240_488)">
     <path d="M4 6C4 3.79086 5.79086 2 8 2H24C26.2091 2 28 3.79086 28 6V18C28 20.2091 26.2091 22 24 22H8C5.79086 22 4 20.2091 4 18V6Z" fill="#FCFCFC"/>
     <path d="M4.5 6C4.5 4.067 6.067 2.5 8 2.5H24C25.933 2.5 27.5 4.067 27.5 6V18C27.5 19.933 25.933 21.5 24 21.5H8C6.067 21.5 4.5 19.933 4.5 18V6Z" stroke="#CECECE"/>
