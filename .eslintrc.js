1ed6bc503690baacbd2547906ec2e5bf7690cd98
paanSinghCoder <gaurav3017@gmail.com>
2025-01-06 12:22:41 +0530

    chore: add ignore pattern in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
+  ignores: ["packages/raystack/dist/**/*/", "packages/raystack/coverage/**/*/"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
