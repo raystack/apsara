f6c54c878500140f63f06c3774a5730d5fc4171f
paanSinghCoder <gaurav3017@gmail.com>
2025-01-09 15:32:09 +0530

    chore: cleanup old code from boilerplate

--- packages/raystack/v1/components/sidepanel/sidepanel.tsx
+++ packages/raystack/v1/components/sidepanel/sidepanel.tsx
@@ -5,39 +5,23 @@ import styles from './sidepanel.module.css';
 
 const sidepanel = cva(styles.sidepanel, {
   variants: {
-    size: {
-      1: styles["sidepanel-size-1"],
-      2: styles["sidepanel-size-2"],
-      3: styles["sidepanel-size-3"],
-      4: styles["sidepanel-size-4"],
-      5: styles["sidepanel-size-5"],
-      6: styles["sidepanel-size-6"],
-    },
-    color: {
-      default: styles["sidepanel-color-default"],
-      inverted: styles["sidepanel-color-inverted"],
-    }
+    
   },
   defaultVariants: {
-    size: 1,
-    color: "default",
+   
   },
 });
 
 export interface SpinnerProps
   extends ComponentPropsWithoutRef<"div">,
     VariantProps<typeof sidepanel> {
-  size?: 1 | 2 | 3 | 4 | 5 | 6;
-  color?: "default" | "inverted";
 }
 
 export const Sidepanel = forwardRef<ElementRef<"div">, SpinnerProps>(
-  ({ className, size, color, ...props }, ref) => (
+  ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={sidepanel({ size, color, className })}
-      role="status"
-      aria-hidden="true"
+      className={sidepanel({ className })}
       {...props}
     >
       {[...Array(8)].map((_, index) => (
