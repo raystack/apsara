f6273374aa430a34f0104112f2b9d414839574d3
paanSinghCoder <gaurav3017@gmail.com>
2025-01-16 17:48:01 +0530

    chore: revert checkbox code

--- packages/raystack/v1/components/badge/badge.tsx
+++ packages/raystack/v1/components/badge/badge.tsx
@@ -32,7 +32,7 @@ export interface BadgeProps
   screenReaderText?: string;
 }
 
-export const Badge = ({ variant = 'accent', size = 'small', icon, children, className, screenReaderText, ref, ...props }: BadgeProps & { ref?: React.Ref<ComponentRef<"span">> }) => (
+export const Badge = ({ className, icon, children, variant, size, screenReaderText, ref, ...props }: BadgeProps & { ref?: React.Ref<ComponentRef<"span">> }) => (
   <span ref={ref} className={badge({ variant, size, className })} {...props}>
     {icon && <span className={styles['icon']}>{icon}</span>}
     {screenReaderText && <span className={styles['sr-only']}>{screenReaderText}</span>}
