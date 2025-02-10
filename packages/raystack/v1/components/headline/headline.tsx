5b24580f7a075a1506f47be0ae3c1ea1d12f2243
paanSinghCoder <gaurav3017@gmail.com>
2025-02-10 14:20:24 +0530

    feat: add initial code

---
+++ packages/raystack/v1/components/headline/headline.tsx
@@ -0,0 +1,49 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { forwardRef, HTMLAttributes } from "react";
+
+import styles from "./headline.module.css";
+
+const headline = cva(styles.headline, {
+  variants: {
+    size: {
+      small: styles["headline-small"],
+      medium: styles["headline-medium"],
+      large: styles["headline-large"],
+    },
+    align: {
+      left: styles["headline-align-left"],
+      center: styles["headline-align-center"],
+      right: styles["headline-align-right"],
+    },
+    truncate: {
+      true: styles["headline-truncate"],
+    }
+  },
+  defaultVariants: {
+    size: "medium",
+    align: "left",
+    truncate: false,
+  },
+});
+
+type HeadlineProps = HTMLAttributes<HTMLHeadingElement> &
+  VariantProps<typeof headline> & {
+    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    "aria-label"?: string;
+  };
+
+export const Headline = forwardRef<HTMLHeadingElement, HeadlineProps>(
+  ({ className, size, align, truncate, as: Component = "h2", children, ...props }, ref) => {
+    return (
+      <Component
+        ref={ref}
+        className={headline({ size, align, truncate, className })}
+        {...props}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
+
+Headline.displayName = "Headline"; 
