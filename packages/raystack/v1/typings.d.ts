562bdd9865b57415f5af0c643afe804a6852e572
Rishabh <me@rsbh.dev>
2025-02-24 11:33:01 +0530

    feat: add svgr plugin

--- packages/raystack/v1/typings.d.ts
+++ packages/raystack/v1/typings.d.ts
@@ -4,6 +4,10 @@ declare module "*.module.css" {
 }
 
 declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
+  import * as React from "react";
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
 }
