3b8b85af4673d7815ffb4b859b093c59c855b487
Rishabh <me@rsbh.dev>
2025-02-27 22:43:13 +0530

    refactor: rename dataTableState to tableQuery

--- packages/raystack/v1/components/data-table/components/search.tsx
+++ packages/raystack/v1/components/data-table/components/search.tsx
@@ -3,12 +3,12 @@ import { SearchProps } from "../../search/search";
 import { useDataTable } from "../hooks/useDataTable";
 
 export function TableSearch({ ...props }: SearchProps) {
-  const { updateTableState } = useDataTable();
+  const { updateTableQuery } = useDataTable();
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    updateTableState((state) => {
+    updateTableQuery((query) => {
       return {
-        ...state,
+        ...query,
         search: value,
       };
     });
