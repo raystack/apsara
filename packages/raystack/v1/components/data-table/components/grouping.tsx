91a80dfa7273e0ead85c5a663c936f50c7a74ddf
Rishabh <me@rsbh.dev>
2025-02-28 09:48:11 +0530

    feat: add config to enable grouping

--- packages/raystack/v1/components/data-table/components/grouping.tsx
+++ packages/raystack/v1/components/data-table/components/grouping.tsx
@@ -2,16 +2,29 @@ import { Flex } from "../../flex";
 import styles from "../data-table.module.css";
 import { Text } from "../../text";
 import { Select } from "../../select";
-import { ColumnData } from "../data-table.types";
+import { DataTableColumn } from "../data-table.types";
 import { defaultGroupOption } from "../utils";
 
-interface GroupingProps {
-  columnList: ColumnData[];
+interface GroupingProps<TData, TValue> {
+  columns: DataTableColumn<TData, TValue>[];
   onChange: (columnId: string) => void;
   value: string;
 }
 
-export function Grouping({ columnList, onChange, value }: GroupingProps) {
+export function Grouping<TData, TValue>({
+  columns = [],
+  onChange,
+  value,
+}: GroupingProps<TData, TValue>) {
+  const columnList = columns
+    .filter((column) => column.columnDef.enableGrouping)
+    ?.map((column) => {
+      const id = column.id;
+      return {
+        label: column.columnDef.header || id,
+        id: id,
+      };
+    });
   return (
     <Flex justify="between" align="center">
       <Text size={2} weight={500} className={styles["flex-1"]}>
