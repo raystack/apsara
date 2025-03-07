84795cbd2e70c4b7b21ea4b4b3d69ea06f679c89
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-07 09:28:23 +0530

    feat: label doc

---
+++ apps/www/src/content/docs/components/label/demo.ts
@@ -0,0 +1,45 @@
+"use client";
+
+import { getPropsString } from "@/lib/utils";
+
+export const getCode = (props: any) => {
+  const { children, ...rest } = props;
+  return `<Label${getPropsString(rest)}>${children}</Indicator>`;
+};
+
+export const playground = {
+  type: "playground",
+  controls: {
+    size: {
+      type: "select",
+      options: ["small", "medium", "large"],
+      defaultValue: "small",
+    },
+    required: { type: "checkbox", defaultValue: false },
+    requiredIndicator: { type: "text", defaultValue: "*" },
+    children: { type: "text", initialValue: "Label" },
+  },
+  getCode,
+};
+
+export const sizeDemo = {
+  type: "code",
+  code: `
+  <Flex gap="large" align="center">
+    <Label size="small">Small Label</Label>
+    <Label size="medium">Medium Label</Label>
+    <Label size="large">Large Label</Label>
+  </Flex>`,
+};
+export const requiredDemo = {
+  type: "code",
+  code: `
+  <Flex gap="large" align="center">
+    <Label size="medium" required>
+      Required Field
+      </Label>
+      <Label size="medium" required requiredIndicator=" (Required)">
+      Required Field
+    </Label>
+  </Flex>`,
+};
