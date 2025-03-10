295050d56b3766646e94f4d966574b3e836d375b
paanSinghCoder <gaurav3017@gmail.com>
2025-03-10 12:28:03 +0530

    fix: dont close popover when select is closed

--- packages/raystack/v1/components/calendar/calendar.tsx
+++ packages/raystack/v1/components/calendar/calendar.tsx
@@ -65,6 +65,7 @@ function DropDown({
         iconProps={{
           className: styles.dropdown_icon,
         }}
+        stopPropagation={true} // Add this prop
       >
         <Select.Value />
       </Select.Trigger>
