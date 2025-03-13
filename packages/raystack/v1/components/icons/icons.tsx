efc4261d2d8612388200ecdee5a8a4d5b49937b7
Rishabh <me@rsbh.dev>
2025-03-13 19:01:24 +0530

    export org sidebar icons

--- packages/raystack/v1/components/icons/icons.tsx
+++ packages/raystack/v1/components/icons/icons.tsx
@@ -1,3 +1,13 @@
 import { ReactComponent as FilterIcon } from "./assets/filter.svg";
+import { ReactComponent as CheckCircleFilledIcon } from "./assets/check-circle-filled.svg";
+import { ReactComponent as CrossCircleFilledIcon } from "./assets/cross-circle-filled.svg";
+import { ReactComponent as SidebarIcon } from "./assets/sidebar.svg";
+import { ReactComponent as OrganizationIcon } from "./assets/organization.svg";
 
-export { FilterIcon };
+export {
+  FilterIcon,
+  CheckCircleFilledIcon,
+  CrossCircleFilledIcon,
+  SidebarIcon,
+  OrganizationIcon,
+};
