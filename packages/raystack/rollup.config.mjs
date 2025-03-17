266924f75e4a9d998a7c2ef0f5d230815eae688c
Rishabh <me@rsbh.dev>
2025-03-17 21:35:11 +0530

    refactor: map over folders to create rollup config

--- packages/raystack/rollup.config.mjs
+++ packages/raystack/rollup.config.mjs
@@ -3,28 +3,26 @@ import image from "@rollup/plugin-image";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import postcssImport from 'postcss-import';
-import svgr from '@svgr/rollup'
+import postcssImport from "postcss-import";
+import svgr from "@svgr/rollup";
 
-const createPlugins = (isV1 = false) => [
+const createPlugins = ({ rootDir, declarationDir }) => [
   nodeResolve(),
   commonjs(),
   svgr(),
   postcss({
-    plugins: [
-      postcssImport(),
-    ],
-    extract: 'style.css',
+    plugins: [postcssImport()],
+    extract: "style.css",
     minimize: true,
     autoModules: true, // Auto process files ending with .module.css
     modules: true,
-    namedExports: true // Enable named exports for CSS modules
+    namedExports: true, // Enable named exports for CSS modules
   }),
   typescript({
     tsconfig: "tsconfig.json",
     declaration: true,
-    rootDir: isV1 ? "v1" : ".",
-    declarationDir: isV1 ? 'dist/v1' : 'dist',
+    rootDir: rootDir,
+    declarationDir: declarationDir,
   }),
   image(),
 ];
@@ -35,62 +33,55 @@ const sharedWarningHandler = (warning, warn) => {
 
   // This ignores the warnings generated during build from
   // CSS module imports which is not standard JS module syntax.
-  if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+  if (warning.code === "MODULE_LEVEL_DIRECTIVE") return;
 
   warn(warning);
 };
 
-export default [
-  // Main bundle
+const configs = [
   {
-    input: "index.tsx",
-    output: [
-      {
-        dir: "dist",
-        format: "es",
-        sourcemap: true,
-        exports: "named",
-        preserveModules: true,
-        preserveModulesRoot: "."
-      },
-      {
-        dir: "dist",
-        format: "cjs",
-        sourcemap: true,
-        exports: "named",
-        entryFileNames: "[name].cjs",
-        preserveModules: true,
-        preserveModulesRoot: "."
-      },
-    ],
-    external: ["react", "react-dom"],
-    plugins: createPlugins(false),
-    onwarn: sharedWarningHandler,
+    inputPath: ".",
+    outputPath: "dist",
   },
-  // V1 bundle
   {
-    input: "v1/index.tsx",
-    output: [
-      {
-        dir: "dist/v1",
-        format: "es",
-        sourcemap: true,
-        exports: "named",
-        preserveModules: true,
-        preserveModulesRoot: "v1"
-      },
-      {
-        dir: "dist/v1",
-        format: "cjs",
-        sourcemap: true,
-        exports: "named",
-        entryFileNames: "[name].cjs",
-        preserveModules: true,
-        preserveModulesRoot: "v1"
-      },
-    ],
-    external: ["react", "react-dom"],
-    plugins: createPlugins(true),
-    onwarn: sharedWarningHandler,
+    inputPath: "v1",
+    outputPath: "dist/v1",
+  },
+  {
+    inputPath: "icons",
+    outputPath: "dist/icons",
   },
 ];
+
+const rollupConfig = configs.map((conf) => {
+  return {
+    input: conf.inputPath + "/index.tsx",
+    output: [
+      {
+        dir: conf.outputPath,
+        format: "es",
+        sourcemap: true,
+        exports: "named",
+        preserveModules: true,
+        preserveModulesRoot: conf.inputPath,
+      },
+      {
+        dir: conf.outputPath,
+        format: "cjs",
+        sourcemap: true,
+        exports: "named",
+        entryFileNames: "[name].cjs",
+        preserveModules: true,
+        preserveModulesRoot: conf.inputPath,
+      },
+    ],
+    external: ["react", "react-dom"],
+    plugins: createPlugins({
+      rootDir: conf.inputPath,
+      declarationDir: conf.outputPath,
+    }),
+    onwarn: sharedWarningHandler,
+  };
+});
+
+export default rollupConfig;
