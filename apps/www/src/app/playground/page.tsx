e13e3852ba20b23ea420c871f83231b8d96bef05
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-18 12:43:21 +0530

    chore: update imports for better readibility

--- apps/www/src/app/playground/page.tsx
+++ apps/www/src/app/playground/page.tsx
@@ -1,92 +1,50 @@
-import {
-  AnnouncementBarExamples,
-  AvatarExamples,
-  BadgeExamples,
-  BreadcrumbExamples,
-  ButtonExamples,
-  CalendarExamples,
-  CalloutExamples,
-  CheckboxExamples,
-  ChipExamples,
-  CommandExamples,
-  ContainerExamples,
-  DataTableExamples,
-  DialogExamples,
-  DropdownMenuExamples,
-  EmptyStateExamples,
-  FlexExamples,
-  FilterChipExamples,
-  HeadlineExamples,
-  IconButtonExamples,
-  ImageExamples,
-  IndicatorExamples,
-  InputFieldExamples,
-  LabelExamples,
-  LinkExamples,
-  ListExamples,
-  PopoverExamples,
-  RadioExamples,
-  SearchExamples,
-  SelectExamples,
-  SeparatorExamples,
-  SheetExamples,
-  SidebarExamples,
-  SliderExamples,
-  SpinnerExamples,
-  SwitchExamples,
-  TableExamples,
-  TabsExamples,
-  TextExamples,
-  TextAreaExamples,
-  ToastExamples,
-  TooltipExamples,
-} from "@/components/playground";
+import * as Playground from "@/components/playground";
 import styles from "./page.module.css";
 
-export default function Playground() {
+export default function Page() {
   return (
     <div className={styles.container}>
-      <AnnouncementBarExamples />
-      <AvatarExamples />
-      <BadgeExamples />
-      <BreadcrumbExamples />
-      <ButtonExamples />
-      <CalendarExamples />
-      <CalloutExamples />
-      <CheckboxExamples />
-      <ChipExamples />
-      <CommandExamples />
-      <ContainerExamples />
-      <DataTableExamples />
-      <DialogExamples />
-      <DropdownMenuExamples />
-      <EmptyStateExamples />
-      <FlexExamples />
-      <FilterChipExamples />
-      <HeadlineExamples />
-      <IconButtonExamples />
-      <ImageExamples />
-      <IndicatorExamples />
-      <InputFieldExamples />
-      <LabelExamples />
-      <LinkExamples />
-      <ListExamples />
-      <PopoverExamples />
-      <RadioExamples />
-      <SearchExamples />
-      <SelectExamples />
-      <SeparatorExamples />
-      <SheetExamples />
-      <SidebarExamples />
-      <SliderExamples />
-      <SpinnerExamples />
-      <SwitchExamples />
-      <TableExamples />
-      <TabsExamples />
-      <TextExamples />
-      <TextAreaExamples />
-      <ToastExamples />
-      <TooltipExamples />
+      <Playground.AnnouncementBarExamples />
+      <Playground.AvatarExamples />
+      <Playground.BadgeExamples />
+      <Playground.BreadcrumbExamples />
+      <Playground.ButtonExamples />
+      <Playground.CalendarExamples />
+      <Playground.CalloutExamples />
+      <Playground.CheckboxExamples />
+      <Playground.ChipExamples />
+      <Playground.CommandExamples />
+      <Playground.ContainerExamples />
+      <Playground.DataTableExamples />
+      <Playground.DialogExamples />
+      <Playground.DropdownMenuExamples />
+      <Playground.EmptyStateExamples />
+      <Playground.FlexExamples />
+      <Playground.FilterChipExamples />
+      <Playground.HeadlineExamples />
+      <Playground.IconButtonExamples />
+      <Playground.ImageExamples />
+      <Playground.IndicatorExamples />
+      <Playground.InputFieldExamples />
+      <Playground.LabelExamples />
+      <Playground.LinkExamples />
+      <Playground.ListExamples />
+      <Playground.PopoverExamples />
+      <Playground.RadioExamples />
+      <Playground.SearchExamples />
+      <Playground.SelectExamples />
+      <Playground.SeparatorExamples />
+      <Playground.SheetExamples />
+      <Playground.SidebarExamples />
+      <Playground.SliderExamples />
+      <Playground.SpinnerExamples />
+      <Playground.SwitchExamples />
+      <Playground.TableExamples />
+      <Playground.TabsExamples />
+      <Playground.TextExamples />
+      <Playground.TextAreaExamples />
+      <Playground.ToastExamples />
+      <Playground.TooltipExamples />
     </div>
   );
 }
