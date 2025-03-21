6a7882fc1f491017b855f9d65a4ac44e56166fa6
Rishabh <me@rsbh.dev>
2025-03-21 11:53:00 +0530

    fix: remove unused code

--- packages/raystack/v1/components/data-table/components/search.tsx
+++ packages/raystack/v1/components/data-table/components/search.tsx
@@ -3,7 +3,7 @@ import { Search } from "../../search";
 import { SearchProps } from "../../search/search";
 import { useDataTable } from "../hooks/useDataTable";
 
-const TableSearchBase = forwardRef<HTMLInputElement, SearchProps>(
+export const TableSearch = forwardRef<HTMLInputElement, SearchProps>(
   ({ ...props }, ref) => {
     const { updateTableQuery, tableQuery } = useDataTable();
 
@@ -37,34 +37,3 @@ const TableSearchBase = forwardRef<HTMLInputElement, SearchProps>(
     );
   }
 );
-export function TableSearch({ ...props }: SearchProps) {
-  const { updateTableQuery, tableQuery } = useDataTable();
-
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    updateTableQuery((query) => {
-      return {
-        ...query,
-        search: value,
-      };
-    });
-  };
-
-  const handleClear = () => {
-    updateTableQuery((query) => {
-      return {
-        ...query,
-        search: "",
-      };
-    });
-  };
-
-  return (
-    <Search
-      {...props}
-      onChange={handleSearch}
-      value={tableQuery?.search}
-      onClear={handleClear}
-    />
-  );
-}
