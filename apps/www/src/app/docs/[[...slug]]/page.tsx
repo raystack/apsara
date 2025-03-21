b76e1755db59be29b92e3b3817f1c55589ed7f6a
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-21 14:44:36 +0530

    feat: add support for tags

--- apps/www/src/app/docs/[[...slug]]/page.tsx
+++ apps/www/src/app/docs/[[...slug]]/page.tsx
@@ -6,12 +6,15 @@ import { TypeTable } from "fumadocs-ui/components/type-table";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import Demo from "@/components/demo";
 import { DocsBody } from "@/components/docs";
+import styles from "./page.module.css";
+import Tag from "@/components/tag";
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
   const params = await props.params;
   const page = docs.getPage(params.slug);
+  console.log("page > ", page);
   if (!page) notFound();
 
   const MDX = page.data.body;
@@ -23,7 +26,12 @@ export default async function Page(props: {
       tableOfContent={{
         single: false,
       }}>
-      <DocsTitle>{page.data.title}</DocsTitle>
+      <DocsTitle>
+        <div className={styles.title}>
+          {page.data.title}
+          <Tag value={page.data.tag} size="regular" />
+        </div>
+      </DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
         <MDX
