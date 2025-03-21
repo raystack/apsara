35dd7541df10dfc18c7a8a213b7663666228d05c
Rishabh <me@rsbh.dev>
2025-03-21 11:49:23 +0530

    fix: forward ref in data table search

--- packages/raystack/v1/components/data-table/components/search.tsx
+++ packages/raystack/v1/components/data-table/components/search.tsx
@@ -1,7 +1,42 @@
+import { forwardRef } from "react";
 import { Search } from "../../search";
 import { SearchProps } from "../../search/search";
 import { useDataTable } from "../hooks/useDataTable";
 
+const TableSearchBase = forwardRef<HTMLInputElement, SearchProps>(
+  ({ ...props }, ref) => {
+    const { updateTableQuery, tableQuery } = useDataTable();
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      updateTableQuery((query) => {
+        return {
+          ...query,
+          search: value,
+        };
+      });
+    };
+
+    const handleClear = () => {
+      updateTableQuery((query) => {
+        return {
+          ...query,
+          search: "",
+        };
+      });
+    };
+
+    return (
+      <Search
+        {...props}
+        ref={ref}
+        onChange={handleSearch}
+        value={tableQuery?.search}
+        onClear={handleClear}
+      />
+    );
+  }
+);
 export function TableSearch({ ...props }: SearchProps) {
   const { updateTableQuery, tableQuery } = useDataTable();
 
