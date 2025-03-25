8360a102836b336162310970964dcd85bde5709b
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-25 22:23:21 +0530

    feat: add icons page (#336)
    * feat: add icons page
    
    * feat: add links for icons
    
    * feat: icons page

---
+++ apps/www/src/app/icons/layout.tsx
@@ -0,0 +1,9 @@
+import { type ReactNode } from "react";
+
+export const metadata = {
+  title: "Apsara Icons",
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
+}
