7b67c4d0131b48c26a1829f1d763653dd08d72b9
Rohan Chakraborty <irohan1999@gmail.com>
2025-04-08 11:04:38 +0530

    chore: add old styles back for Text

--- packages/raystack/v1/components/text/text.tsx
+++ packages/raystack/v1/components/text/text.tsx
@@ -21,10 +21,33 @@ export const textVariants = cva(styles.text, {
       small: styles["text-small"],
       regular: styles["text-regular"],
       large: styles["text-large"],
+      1: styles["text-1"],
+      2: styles["text-2"],
+      3: styles["text-3"],
+      4: styles["text-4"],
+      5: styles["text-5"],
+      6: styles["text-6"],
+      7: styles["text-7"],
+      8: styles["text-8"],
+      9: styles["text-9"],
+      10: styles["text-10"],
     },
     weight: {
       regular: styles["text-weight-regular"],
       medium: styles["text-weight-medium"],
+      bold: styles["text-weight-bold"],
+      bolder: styles["text-weight-bolder"],
+      normal: styles["text-weight-normal"],
+      lighter: styles["text-weight-lighter"],
+      100: styles["text-weight-100"],
+      200: styles["text-weight-200"],
+      300: styles["text-weight-300"],
+      400: styles["text-weight-400"],
+      500: styles["text-weight-500"],
+      600: styles["text-weight-600"],
+      700: styles["text-weight-700"],
+      800: styles["text-weight-800"],
+      900: styles["text-weight-900"],
     },
     transform: {
       capitalize: styles["text-transform-capitalize"],
@@ -56,7 +79,7 @@ export const textVariants = cva(styles.text, {
   },
   defaultVariants: {
     variant: "primary",
-    size: "regular",
+    size: "small",
     weight: "regular",
   },
   compoundVariants: [
@@ -68,7 +91,46 @@ export const textVariants = cva(styles.text, {
   ],
 });
 
-export type TextBaseProps = VariantProps<typeof textVariants>;
+export type TextBaseProps = VariantProps<typeof textVariants> & {
+  /**
+   * @remarks Use "micro", "mini", "small", "regular", "large"
+   */
+  size?:
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | "micro"
+    | "mini"
+    | "small"
+    | "regular"
+    | "large";
+  /**
+   * @remarks Use "regular", "medium"
+   */
+  weight?:
+    | "regular"
+    | "medium"
+    | "bold"
+    | "bolder"
+    | "normal"
+    | "lighter"
+    | 100
+    | 200
+    | 300
+    | 400
+    | 500
+    | 600
+    | 700
+    | 800
+    | 900;
+};
 
 type TextSpanProps = { as?: "span" } & ComponentPropsWithoutRef<"span">;
 type TextDivProps = { as: "div" } & ComponentPropsWithoutRef<"div">;
