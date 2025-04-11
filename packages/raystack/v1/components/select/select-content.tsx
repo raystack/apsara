c496c4035839f0c7696b42d402360537add263b6
Rohan Chakraborty <irohan1999@gmail.com>
2025-04-11 11:01:40 +0530

    feat: improve select icon structure

---
+++ packages/raystack/v1/components/select/select-content.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { cx } from "class-variance-authority";
+import styles from "./select.module.css";
+
+export const SelectContent = forwardRef<
+  ElementRef<typeof SelectPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({ className, children, position = "popper", ...props }, ref) => (
+  <SelectPrimitive.Portal>
+    <SelectPrimitive.Content
+      ref={ref}
+      className={cx(styles.content, className)}
+      position={position}
+      onPointerDownOutside={e => {
+        e.stopPropagation();
+      }}
+      {...props}>
+      {children}
+    </SelectPrimitive.Content>
+  </SelectPrimitive.Portal>
+));
+SelectContent.displayName = SelectPrimitive.Content.displayName;
