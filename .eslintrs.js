c6ce6fb379126e28def485a5ab45193d032c1833
paanSinghCoder <gaurav3017@gmail.com>
2025-05-05 14:11:54 +0530

    feat: setup biome

---
+++ .eslintrs.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // This project uses Biome instead of ESLint
+  // Keep this file for backwards compatibility and to avoid eslint conflicts
+  root: true,
+  extends: [],
+  rules: {},
+};
