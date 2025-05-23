0cc2f59736bfb83276bfbaaf2eca3b19bada27a6
Rohan Chakraborty <irohan1999@gmail.com>
2025-05-23 17:53:06 +0530

    feat: breadcrumb `as` support (#410)
    * feat: breadcrumb as support
    
    * feat: update docs

--- packages/raystack/v1/components/breadcrumb/breadcrumb.tsx
+++ packages/raystack/v1/components/breadcrumb/breadcrumb.tsx
@@ -1,18 +1,90 @@
 import { ChevronDownIcon, DotsHorizontalIcon } from '@radix-ui/react-icons';
-import { type VariantProps, cva } from 'class-variance-authority';
-import React, { forwardRef, PropsWithChildren } from 'react';
-
+import { type VariantProps, cva, cx } from 'class-variance-authority';
+import {
+  MouseEvent,
+  ReactElement,
+  cloneElement,
+  forwardRef,
+  useMemo
+} from 'react';
 import { DropdownMenu } from '../dropdown-menu';
 import styles from './breadcrumb.module.css';
 
-interface BreadcrumbItem {
+interface BreadcrumbItemBase {
   label: string;
-  href: string;
+  href?: string;
+}
+interface BreadcrumbItem extends BreadcrumbItemBase {
   icon?: React.ReactNode;
-  dropdownItems?: Array<{ label: string; href: string }>;
+  dropdownItems?: BreadcrumbItemBase[];
+  as?: ReactElement;
 }
 
-const breadcrumb = cva(styles['breadcrumb'], {
+interface BreadcrumbItemProps extends BreadcrumbItem {
+  isActive: boolean;
+  onItemClick?: (item: BreadcrumbItem) => void;
+}
+
+const BreadcrumbItem = ({
+  as = <a />,
+  onItemClick,
+  ...props
+}: BreadcrumbItemProps) => {
+  const { label, href, icon, dropdownItems, isActive } = props;
+
+  if (label === 'ellipsis') {
+    return (
+      <span className={styles['breadcrumb-ellipsis']}>
+        <DotsHorizontalIcon />
+      </span>
+    );
+  }
+  if (dropdownItems) {
+    return (
+      <DropdownMenu>
+        <DropdownMenu.Trigger className={styles['breadcrumb-dropdown-trigger']}>
+          {icon && <span className={styles['breadcrumb-icon']}>{icon}</span>}
+          <span>{label}</span>
+          <ChevronDownIcon className={styles['breadcrumb-dropdown-icon']} />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className={styles['breadcrumb-dropdown-content']}>
+          {dropdownItems.map((dropdownItem, dropdownIndex) => (
+            <DropdownMenu.Item
+              key={dropdownIndex}
+              className={styles['breadcrumb-dropdown-item']}
+              onSelect={() => onItemClick?.(dropdownItem)}
+            >
+              {dropdownItem.label}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    );
+  }
+  return cloneElement(
+    as,
+    {
+      className: cx(
+        styles['breadcrumb-link'],
+        isActive && styles['breadcrumb-link-active']
+      ),
+      onClick: (e: MouseEvent) => {
+        if (onItemClick && href !== '#') {
+          e.preventDefault();
+          onItemClick(props);
+        }
+      },
+      href,
+      ...as.props
+    },
+    <>
+      {icon && <span className={styles['breadcrumb-icon']}>{icon}</span>}
+      <span>{label}</span>
+    </>
+  );
+};
+
+const breadcrumbVariants = cva(styles['breadcrumb'], {
   variants: {
     size: {
       small: styles['breadcrumb-small'],
@@ -24,9 +96,7 @@ const breadcrumb = cva(styles['breadcrumb'], {
   }
 });
 
-type BreadcrumbProps = PropsWithChildren<
-  Omit<VariantProps<typeof breadcrumb>, 'size'>
-> & {
+type BreadcrumbProps = VariantProps<typeof breadcrumbVariants> & {
   items: BreadcrumbItem[];
   maxVisibleItems?: number;
   separator?: React.ReactNode;
@@ -48,89 +118,41 @@ export const Breadcrumb = forwardRef<HTMLDivElement, BreadcrumbProps>(
     },
     ref
   ) => {
-    const visibleItems =
-      maxVisibleItems && items.length > maxVisibleItems
-        ? [
-            ...items.slice(0, 1),
-            { label: 'ellipsis', href: '#' },
-            ...items.slice(-Math.min(maxVisibleItems - 1, items.length - 1))
-          ]
-        : items;
-
-    const renderItem = (
-      item: BreadcrumbItem,
-      index: number,
-      isLast: boolean
-    ) => (
-      <li key={index} className={styles['breadcrumb-item']}>
-        {item.label === 'ellipsis' ? (
-          <span className={styles['breadcrumb-ellipsis']}>
-            <DotsHorizontalIcon />
-          </span>
-        ) : item?.dropdownItems ? (
-          <DropdownMenu>
-            <DropdownMenu.Trigger
-              className={styles['breadcrumb-dropdown-trigger']}
-            >
-              {item.icon && (
-                <span className={styles['breadcrumb-icon']}>{item.icon}</span>
-              )}
-              <span>{item.label}</span>
-              <ChevronDownIcon className={styles['breadcrumb-dropdown-icon']} />
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content
-              className={styles['breadcrumb-dropdown-content']}
-            >
-              {item.dropdownItems.map((dropdownItem, dropdownIndex) => (
-                <DropdownMenu.Item
-                  key={dropdownIndex}
-                  className={styles['breadcrumb-dropdown-item']}
-                  onClick={() =>
-                    onItemClick &&
-                    onItemClick({
-                      label: dropdownItem.label,
-                      href: dropdownItem.href
-                    })
-                  }
-                >
-                  {dropdownItem.label}
-                </DropdownMenu.Item>
-              ))}
-            </DropdownMenu.Content>
-          </DropdownMenu>
-        ) : (
-          <a
-            href={item.href}
-            className={`${styles['breadcrumb-link']} ${isLast ? styles['breadcrumb-link-active'] : ''}`}
-            onClick={e => {
-              if (onItemClick && item.href !== '#') {
-                e.preventDefault();
-                onItemClick(item);
-              }
-            }}
-          >
-            {item.icon && (
-              <span className={styles['breadcrumb-icon']}>{item.icon}</span>
-            )}
-            <span>{item.label}</span>
-          </a>
-        )}
-        {!isLast && (
-          <span className={styles['breadcrumb-separator']}>{separator}</span>
-        )}
-      </li>
-    );
+    const visibleItems = useMemo(() => {
+      if (maxVisibleItems && items.length > maxVisibleItems) {
+        return [
+          ...items.slice(0, 1),
+          { label: 'ellipsis', href: '#' },
+          ...items.slice(-Math.min(maxVisibleItems - 1, items.length - 1))
+        ];
+      }
+      return items;
+    }, [maxVisibleItems, items]);
 
     return (
       <nav
-        className={breadcrumb({ size: size as 'small' | 'medium', className })}
+        className={breadcrumbVariants({ size, className })}
         ref={ref}
         {...props}
       >
         <ol className={styles['breadcrumb-list']}>
-          {visibleItems.map((item, index) =>
-            renderItem(item, index, index === visibleItems.length - 1)
-          )}
+          {visibleItems.map((item, index) => {
+            const isLast = index === visibleItems.length - 1;
+            return (
+              <li className={styles['breadcrumb-item']} key={index}>
+                <BreadcrumbItem
+                  {...item}
+                  isActive={isLast}
+                  onItemClick={onItemClick}
+                />
+                {!isLast && (
+                  <span className={styles['breadcrumb-separator']}>
+                    {separator}
+                  </span>
+                )}
+              </li>
+            );
+          })}
         </ol>
       </nav>
     );
