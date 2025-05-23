f5606f2e0cf70611cb5dc1effe27f578a61c272e
Rohan Chakraborty <irohan1999@gmail.com>
2025-05-23 17:47:44 +0530

    feat: tooltip arrow style changes

--- packages/raystack/v1/components/tooltip/tooltip.tsx
+++ packages/raystack/v1/components/tooltip/tooltip.tsx
@@ -1,26 +1,24 @@
-import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-import { cva, VariantProps } from "class-variance-authority";
-import clsx from "clsx";
-import React from "react";
-
-import { Text } from "../text";
-import styles from "./tooltip.module.css";
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { VariantProps, cva, cx } from 'class-variance-authority';
+import React, { useId } from 'react';
+import { Text } from '../text';
+import styles from './tooltip.module.css';
 
 const tooltip = cva(styles.content, {
   variants: {
     side: {
-      top: styles["side-top"],
-      right: styles["side-right"],
-      bottom: styles["side-bottom"],
-      left: styles["side-left"],
-      "top-left": styles["side-top-left"],
-      "top-right": styles["side-top-right"],
-      "bottom-left": styles["side-bottom-left"],
-      "bottom-right": styles["side-bottom-right"],
+      top: styles['side-top'],
+      right: styles['side-right'],
+      bottom: styles['side-bottom'],
+      left: styles['side-left'],
+      'top-left': styles['side-top-left'],
+      'top-right': styles['side-top-right'],
+      'bottom-left': styles['side-bottom-left'],
+      'bottom-right': styles['side-bottom-right']
     }
   },
   defaultVariants: {
-    side: "top"
+    side: 'top'
   }
 });
 
@@ -39,6 +37,7 @@ interface TooltipProps extends VariantProps<typeof tooltip> {
   skipDelayDuration?: number;
   'aria-label'?: string;
   asChild?: boolean;
+  id?: string;
   showArrow?: boolean;
 }
 
@@ -46,7 +45,7 @@ export const Tooltip: React.FC<TooltipProps> = ({
   children,
   message,
   disabled,
-  side = "top",
+  side = 'top',
   classNames,
   triggerStyle,
   contentStyle,
@@ -55,41 +54,59 @@ export const Tooltip: React.FC<TooltipProps> = ({
   'aria-label': ariaLabel,
   asChild = true,
   showArrow = true,
+  id
 }) => {
-  return disabled ? (
-    children
-  ) : (
-    <TooltipPrimitive.Provider delayDuration={delayDuration} skipDelayDuration={skipDelayDuration}>
+  const generatedId = useId();
+  const tooltipId = id ?? generatedId;
+
+  if (disabled) return children;
+
+  return (
+    <TooltipPrimitive.Provider
+      delayDuration={delayDuration}
+      skipDelayDuration={skipDelayDuration}
+    >
       <TooltipPrimitive.Root>
-        <TooltipPrimitive.Trigger 
-          aria-describedby="tooltip" 
+        <TooltipPrimitive.Trigger
+          aria-describedby={tooltipId}
           asChild={asChild}
         >
-          <div className={clsx(styles.trigger, classNames?.trigger ?? "")} style={triggerStyle}>
+          <div
+            className={cx(styles.trigger, classNames?.trigger)}
+            style={triggerStyle}
+          >
             {children}
           </div>
         </TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
-            id="tooltip"
-            role="tooltip"
-            aria-label={ariaLabel || (typeof message === 'string' ? message : undefined)}
-            side={side?.split('-')[0] as TooltipPrimitive.TooltipContentProps['side'] || 'top'}
-            align={(side?.includes('-') ? (side.split('-')[1] === 'left' ? 'start' : 'end') : 'center') satisfies 'start' | 'end' | 'center'}
+            id={tooltipId}
+            role='tooltip'
+            aria-label={
+              ariaLabel || (typeof message === 'string' ? message : undefined)
+            }
+            side={
+              (side?.split(
+                '-'
+              )[0] as TooltipPrimitive.TooltipContentProps['side']) || 'top'
+            }
+            align={
+              (side?.includes('-')
+                ? side.split('-')[1] === 'left'
+                  ? 'start'
+                  : 'end'
+                : 'center') satisfies 'start' | 'end' | 'center'
+            }
             sideOffset={4}
             className={tooltip({ side, className: classNames?.content })}
             style={contentStyle}
           >
-            {typeof message === "string" ? (
-              <Text>{message}</Text>
-            ) : (
-              message
-            )}
+            {typeof message === 'string' ? <Text>{message}</Text> : message}
             {showArrow && (
-              <TooltipPrimitive.Arrow 
-                className={clsx(styles.arrow, classNames?.arrow)} 
-                width={12} 
-                height={6} 
+              <TooltipPrimitive.Arrow
+                className={cx(styles.arrow, classNames?.arrow)}
+                width={7}
+                height={7}
               />
             )}
           </TooltipPrimitive.Content>
