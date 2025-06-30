cc14ad642a3e26436009c56b49ebd6ff9dcb7c92
Rishabh Mishra <me@rsbh.dev>
2025-06-30 12:14:51 +0530

    fix: table loader skeleton (#444)
    * refactor: use local skeleton component in the table
    
    * feat: add width props to flex component

--- packages/raystack/v1/components/flex/flex.tsx
+++ packages/raystack/v1/components/flex/flex.tsx
@@ -1,57 +1,60 @@
-import { cva, VariantProps } from "class-variance-authority";
-import { forwardRef, HTMLAttributes, PropsWithChildren } from "react";
+import { VariantProps, cva } from 'class-variance-authority';
+import { HTMLAttributes, PropsWithChildren, forwardRef } from 'react';
 
-import styles from "./flex.module.css";
+import styles from './flex.module.css';
 
 const flex = cva(styles.flex, {
   variants: {
     direction: {
-      row: styles["direction-row"],
-      column: styles["direction-column"],
-      rowReverse: styles["direction-rowReverse"],
-      columnReverse: styles["direction-columnReverse"],
+      row: styles['direction-row'],
+      column: styles['direction-column'],
+      rowReverse: styles['direction-rowReverse'],
+      columnReverse: styles['direction-columnReverse']
     },
     align: {
-      start: styles["align-start"],
-      center: styles["align-center"],
-      end: styles["align-end"],
-      stretch: styles["align-stretch"],
-      baseline: styles["align-baseline"],
+      start: styles['align-start'],
+      center: styles['align-center'],
+      end: styles['align-end'],
+      stretch: styles['align-stretch'],
+      baseline: styles['align-baseline']
     },
     justify: {
-      start: styles["justify-start"],
-      center: styles["justify-center"],
-      end: styles["justify-end"],
-      between: styles["justify-between"],
+      start: styles['justify-start'],
+      center: styles['justify-center'],
+      end: styles['justify-end'],
+      between: styles['justify-between']
     },
     wrap: {
-      noWrap: styles["wrap-noWrap"],
-      wrap: styles["wrap-wrap"],
-      wrapReverse: styles["wrap-wrapReverse"],
+      noWrap: styles['wrap-noWrap'],
+      wrap: styles['wrap-wrap'],
+      wrapReverse: styles['wrap-wrapReverse']
     },
     gap: {
-      1: styles["gap-1"],
-      2: styles["gap-2"],
-      3: styles["gap-3"],
-      4: styles["gap-4"],
-      5: styles["gap-5"],
-      6: styles["gap-6"],
-      7: styles["gap-7"],
-      8: styles["gap-8"],
-      9: styles["gap-9"],
-      "extra-small": styles["gap-xs"],
-      small: styles["gap-sm"],
-      medium: styles["gap-md"],
-      large: styles["gap-lg"],
-      "extra-large": styles["gap-xl"],
+      1: styles['gap-1'],
+      2: styles['gap-2'],
+      3: styles['gap-3'],
+      4: styles['gap-4'],
+      5: styles['gap-5'],
+      6: styles['gap-6'],
+      7: styles['gap-7'],
+      8: styles['gap-8'],
+      9: styles['gap-9'],
+      'extra-small': styles['gap-xs'],
+      small: styles['gap-sm'],
+      medium: styles['gap-md'],
+      large: styles['gap-lg'],
+      'extra-large': styles['gap-xl']
     },
+    width: {
+      full: styles['width-full']
+    }
   },
   defaultVariants: {
-    direction: "row",
-    align: "stretch",
-    justify: "start",
-    wrap: "noWrap",
-  },
+    direction: 'row',
+    align: 'stretch',
+    justify: 'start',
+    wrap: 'noWrap'
+  }
 });
 
 type BoxProps = PropsWithChildren<VariantProps<typeof flex>> &
@@ -59,12 +62,30 @@ type BoxProps = PropsWithChildren<VariantProps<typeof flex>> &
 
 export const Flex = forwardRef<HTMLDivElement, BoxProps>(
   (
-    { children, direction, align, justify, wrap, gap, className, ...props },
+    {
+      children,
+      direction,
+      align,
+      justify,
+      wrap,
+      gap,
+      className,
+      width,
+      ...props
+    },
     ref
   ) => {
     return (
       <div
-        className={flex({ direction, align, justify, wrap, gap, className })}
+        className={flex({
+          direction,
+          align,
+          justify,
+          wrap,
+          gap,
+          className,
+          width
+        })}
         {...props}
         ref={ref}
       >
