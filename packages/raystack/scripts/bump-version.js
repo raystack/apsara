4069938c6f5bec5307fcc863f8ce6beeb01e5c38
Rohan Chakraborty <irohan1999@gmail.com>
2025-07-01 11:57:26 +0530

    fix: bump scripts (#455)
    * fix: bump version script
    
    * chore: update package

--- packages/raystack/scripts/bump-version.js
+++ packages/raystack/scripts/bump-version.js
@@ -1,21 +1,24 @@
-const semver = require("semver")
-const fs = require("fs/promises")
-const path = require("path")
+const semver = require('semver');
+const fs = require('fs/promises');
+const path = require('path');
 
-const pkg = require("../../package.json")
+const pkg = require('../package.json');
 
 async function updatePackageVersion() {
   try {
-    const gitRef = process.env.GIT_REFNAME
+    const gitRef = process.env.GIT_REFNAME;
     const gitTag = semver.valid(gitRef);
     if (gitTag && semver.compare(gitTag, pkg.version) > 0) {
       pkg.version = gitTag;
-      console.log('Bumped version to ', gitTag)
-      await fs.writeFile(path.join(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2))
+      console.log('Bumped version to ', gitTag);
+      await fs.writeFile(
+        path.join(process.cwd(), 'package.json'),
+        JSON.stringify(pkg, null, 2)
+      );
     }
   } catch (err) {
-    console.error("Update Package Version error: ", err)
+    console.error('Update Package Version error: ', err);
   }
 }
 
-updatePackageVersion()
+updatePackageVersion();
