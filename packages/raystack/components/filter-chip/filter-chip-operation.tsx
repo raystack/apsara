54e7e5027307c78e41955910bbbb261edda2536c
Rohan Chakraborty <irohan1999@gmail.com>
2025-07-07 14:35:27 +0530

    feat: support for custom operations in FilterChip (#458)
    * feat: add support for custom operations in filterchip
    
    * feat: update filterchip docs
    
    * feat: fix unknown columnType
    
    * chore: update doc

---
+++ packages/raystack/components/filter-chip/filter-chip-operation.tsx
@@ -0,0 +1,68 @@
+import { useCallback } from 'react';
+import { FilterOperation, FilterOperator } from '~/types/filters';
+import { Select } from '../select';
+import { Text } from '../text';
+import styles from './filter-chip.module.css';
+
+interface OperationProps {
+  label: string;
+  value?: FilterOperation;
+  onChange: (operation: FilterOperation) => void;
+  operations: FilterOperator<string>[];
+}
+
+export const Operation = ({
+  label,
+  value,
+  onChange,
+  operations
+}: OperationProps) => {
+  const operationValue = value?.value ?? '';
+
+  const handleValueChange = useCallback(
+    (newValue: string) => {
+      const operation = operations.find(o => o.value === newValue);
+      if (operation) onChange(operation);
+    },
+    [operations, onChange]
+  );
+
+  if (operations.length === 1)
+    return (
+      <Text variant='secondary' className={styles.selectValue}>
+        {operations[0].label}
+      </Text>
+    );
+
+  return (
+    <Select
+      value={operationValue}
+      onValueChange={handleValueChange}
+      aria-labelledby={`${label}-label`}
+    >
+      <Select.Trigger
+        variant='text'
+        className={styles.selectValue}
+        aria-label={`${label} filter operation`}
+      >
+        <Select.Value
+          placeholder='Select operation'
+          className={styles.operationText}
+        />
+      </Select.Trigger>
+      <Select.Content data-variant='filter'>
+        {operations.map(operation => {
+          return (
+            <Select.Item
+              key={operation.value}
+              value={operation.value}
+              aria-label={`Filter ${label} ${operation.label}`}
+            >
+              {operation.label}
+            </Select.Item>
+          );
+        })}
+      </Select.Content>
+    </Select>
+  );
+};
