7b51081af6eb0b40c5504d4b954a5b750ae211d8
Rohan Chakraborty <irohan1999@gmail.com>
2025-07-23 21:19:08 +0530

    chore: fix doc usage, examples and redundant tags

--- apps/www/src/content/docs/components/command/demo.ts
+++ apps/www/src/content/docs/components/command/demo.ts
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
 export const preview = {
-  type: "code",
+  type: 'code',
   code: `
   <Flex style={{width:400}}>
     <Command>
@@ -21,5 +21,30 @@ export const preview = {
         </Command.Group>
       </Command.List>
     </Command>
-  </Flex>`,
+  </Flex>`
+};
+
+export const basicDemo = {
+  type: 'code',
+  code: `
+  <Flex style={{ width: 400 }}>
+  <Command>
+    <Command.Input placeholder="Type a command or search..." />
+    <Command.List>
+      <Command.Empty>No results found.</Command.Empty>
+      <Command.Group heading="Suggestions">
+        <Command.Item>Calendar</Command.Item>
+        <Command.Item>Search Emoji</Command.Item>
+        <Command.Item>Calculator</Command.Item>
+      </Command.Group>
+      <Command.Separator />
+      <Command.Group heading="Settings">
+        <Command.Item>Profile</Command.Item>
+        <Command.Item>Billing</Command.Item>
+        <Command.Item>Settings</Command.Item>
+      </Command.Group>
+    </Command.List>
+  </Command>
+</Flex>
+  `
 };
