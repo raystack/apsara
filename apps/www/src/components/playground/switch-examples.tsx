441b589b352f9958ceb662e05084af6226f824a0
Rohan Chakraborty <irohan1999@gmail.com>
2025-08-25 09:46:32 +0530

    feat: add size variant for switch (#508)

--- apps/www/src/components/playground/switch-examples.tsx
+++ apps/www/src/components/playground/switch-examples.tsx
@@ -1,17 +1,31 @@
-"use client";
+'use client';
 
-import { Switch, Flex } from "@raystack/apsara";
-import PlaygroundLayout from "./playground-layout";
+import { Flex, Switch, Text } from '@raystack/apsara';
+import PlaygroundLayout from './playground-layout';
 
 export function SwitchExamples() {
   return (
-    <PlaygroundLayout title="Switch">
-      <Flex gap="large" align="center" wrap="wrap">
+    <PlaygroundLayout title='Switch'>
+      <Flex gap='large' align='center' wrap='wrap'>
         <Switch />
         <Switch defaultChecked />
         <Switch disabled />
         <Switch disabled defaultChecked />
         <Switch required />
+      </Flex>
+
+      <Flex gap='large' align='center' wrap='wrap'>
+        <Text>Large (default):</Text>
+        <Switch size='large' />
+        <Switch size='large' defaultChecked />
+      </Flex>
+
+      <Flex gap='large' align='center' wrap='wrap'>
+        <Text>Small:</Text>
+        <Switch size='small' />
+        <Switch size='small' defaultChecked />
+        <Switch size='small' disabled />
+        <Switch size='small' disabled defaultChecked />
       </Flex>
     </PlaygroundLayout>
   );
--- apps/www/src/content/docs/components/switch/demo.ts
+++ apps/www/src/content/docs/components/switch/demo.ts
@@ -1,28 +1,33 @@
-"use client";
+'use client';
 
-import { getPropsString } from "@/lib/utils";
+import { getPropsString } from '@/lib/utils';
 
 export const getCode = (props: any) => {
   return `<Switch${getPropsString(props)}/>`;
 };
 
 export const playground = {
-  type: "playground",
+  type: 'playground',
   controls: {
     defaultChecked: {
-      type: "checkbox",
-      defaultValue: false,
+      type: 'checkbox',
+      defaultValue: false
     },
     disabled: {
-      type: "checkbox",
-      defaultValue: false,
+      type: 'checkbox',
+      defaultValue: false
     },
+    size: {
+      type: 'select',
+      options: ['large', 'small'],
+      defaultValue: 'large'
+    }
   },
-  getCode,
+  getCode
 };
 
 export const stateDemo = {
-  type: "code",
+  type: 'code',
   code: `
   <Flex gap="large" align="center">
     <Switch />
@@ -30,18 +35,30 @@ export const stateDemo = {
     <Switch disabled />
     <Switch disabled defaultChecked />
     <Switch required />
-  </Flex>`,
+  </Flex>`
 };
+
+export const sizeDemo = {
+  type: 'code',
+  code: `
+  <Flex gap="large" align="center">
+    <Switch size="large" />
+    <Switch size="large" defaultChecked />
+    <Switch size="small" />
+    <Switch size="small" defaultChecked />
+  </Flex>`
+};
+
 export const controlDemo = {
-  type: "code",
+  type: 'code',
   code: `
   function ControlledSwitch() {
   const [checked, setChecked] = React.useState(false);
   return (
-    <Switch 
+    <Switch
       checked={checked}
       onCheckedChange={setChecked}
     />
   );
-}`,
+}`
 };
