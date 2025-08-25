f6a34db76261dd342dff98a2e1470319dfa28244
Rishabh <me@rsbh.dev>
2025-08-25 11:44:51 +0530

    chore: setup vitest

---
+++ packages/raystack/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+    css: {
+      modules: {
+        classNameStrategy: 'stable'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '~/': new URL('./', import.meta.url).pathname,
+    }
+  }
+});
