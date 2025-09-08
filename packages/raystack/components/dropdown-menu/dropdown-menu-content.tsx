d82cbbcee646a9420b6763bea12c630c7647ae61
Rohan Chakraborty <irohan1999@gmail.com>
2025-09-08 10:20:55 +0530

    fix: dropdown potal fix when used inside dialog

--- packages/raystack/components/dropdown-menu/dropdown-menu-content.tsx
+++ packages/raystack/components/dropdown-menu/dropdown-menu-content.tsx
@@ -3,8 +3,8 @@
 import { Menu, MenuProps, useMenuContext } from '@ariakit/react';
 import { Combobox, ComboboxList } from '@ariakit/react';
 import { cx } from 'class-variance-authority';
-import { Slot } from 'radix-ui';
-import { ElementRef, forwardRef, useRef } from 'react';
+import { Slot, VisuallyHidden } from 'radix-ui';
+import { ElementRef, forwardRef, useEffect, useRef, useState } from 'react';
 import { useDropdownContext } from './dropdown-menu-root';
 import styles from './dropdown-menu.module.css';
 import { WithAsChild } from './types';
@@ -25,45 +25,65 @@ export const DropdownMenuContent = forwardRef<
     const { autocomplete } = useDropdownContext();
     const isSubMenu = !!menu?.parent;
     const comboboxRef = useRef<HTMLInputElement>(null);
+    const visuallyHiddenRef = useRef<HTMLDivElement>(null);
+    const [isInsideRadixDialog, setIsInsideRadixDialog] = useState(false);
+
+    /*
+     * This is a workaround to fix focus lock issue when the dropdown menu is inside a Radix Dialog.
+     * TODO: Use Radix.Popover for the dropdown popover
+     */
+    useEffect(() => {
+      setIsInsideRadixDialog(
+        !!visuallyHiddenRef.current?.closest("[role='dialog']")
+      );
+    }, []);
 
     return (
-      <Menu
-        ref={ref}
-        modal
-        portal
-        portalElement={
-          typeof window === 'undefined' ? null : window?.document?.body
-        }
-        unmountOnHide
-        gutter={isSubMenu ? 2 : 4}
-        className={cx(
-          styles.content,
-          autocomplete && styles.comboboxContainer,
-          className
-        )}
-        render={asChild ? <Slot.Root /> : undefined}
-        {...props}
-      >
-        {autocomplete ? (
-          <>
-            <Combobox
-              ref={comboboxRef}
-              placeholder={searchPlaceholder}
-              className={styles.comboboxInput}
-              autoSelect
-              onPointerEnter={e => {
-                if (document && document.activeElement !== comboboxRef.current)
-                  comboboxRef.current?.focus();
-              }}
-            />
-            <ComboboxList className={styles.comboboxContent}>
-              {children}
-            </ComboboxList>
-          </>
-        ) : (
-          children
-        )}
-      </Menu>
+      <>
+        <VisuallyHidden.Root ref={visuallyHiddenRef} aria-hidden='true' />
+        <Menu
+          ref={ref}
+          modal={!isInsideRadixDialog}
+          portal={!isInsideRadixDialog}
+          portalElement={
+            typeof window === 'undefined' || isInsideRadixDialog
+              ? null
+              : window?.document?.body
+          }
+          unmountOnHide
+          gutter={isSubMenu ? 2 : 4}
+          className={cx(
+            styles.content,
+            autocomplete && styles.comboboxContainer,
+            className
+          )}
+          render={asChild ? <Slot.Root /> : undefined}
+          {...props}
+        >
+          {autocomplete ? (
+            <>
+              <Combobox
+                ref={comboboxRef}
+                placeholder={searchPlaceholder}
+                className={styles.comboboxInput}
+                autoSelect
+                onPointerEnter={e => {
+                  if (
+                    document &&
+                    document.activeElement !== comboboxRef.current
+                  )
+                    comboboxRef.current?.focus();
+                }}
+              />
+              <ComboboxList className={styles.comboboxContent}>
+                {children}
+              </ComboboxList>
+            </>
+          ) : (
+            children
+          )}
+        </Menu>
+      </>
     );
   }
 );
