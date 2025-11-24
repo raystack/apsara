0de6f186b1d4bdc15bf08cca4c90fa45a2491ad7
Rohan Chakraborty <irohan1999@gmail.com>
2025-11-24 09:54:45 +0530

    feat: toc

--- apps/www/src/app/docs/[[...slug]]/page.tsx
+++ apps/www/src/app/docs/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import DocsNavbar from '@/components/docs/navbar';
 import { mdxComponents } from '@/components/mdx';
+import TableOfContents from '@/components/toc/toc';
 import { docs } from '@/lib/source';
 import { Flex, Headline, Text } from '@raystack/apsara';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
@@ -14,34 +15,52 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
 
   const MDX = page.data.body;
 
+  console.log('page', page);
+
   return (
     <Flex
       direction='column'
       justify='center'
       align='center'
       className={styles.container}
+      data-article-content
     >
       <DocsNavbar
         url={page.url}
         title={page.data.title}
         pageTree={docs.pageTree}
       />
-      <Flex direction='column' gap={6} className={styles.content}>
-        <Flex direction='column' gap={3}>
-          <Headline size='t4'>{page.data.title}</Headline>
-          <Text size='regular' variant='secondary'>
-            {page.data.description}
-          </Text>
+      <Flex width='full'>
+        <Flex direction='column' align='center' justify='center' width='full'>
+          <Flex direction='column' gap={6} className={styles.content}>
+            <Flex direction='column' gap={3}>
+              <Headline size='t4'>{page.data.title}</Headline>
+              <Text size='regular' variant='secondary'>
+                {page.data.description}
+              </Text>
+            </Flex>
+            <Flex direction='column' className='prose'>
+              <MDX
+                components={{
+                  ...mdxComponents,
+                  // this allows you to link to other pages with relative file paths
+                  a: createRelativeLink(docs, page)
+                }}
+              />
+            </Flex>
+          </Flex>
         </Flex>
-        <Flex direction='column' className='prose'>
-          <MDX
-            components={{
-              ...mdxComponents,
-              // this allows you to link to other pages with relative file paths
-              a: createRelativeLink(docs, page)
-            }}
-          />
-        </Flex>
+        <aside
+          style={{
+            width: '300px',
+            height: 'calc(100vh - 50px)',
+            position: 'sticky',
+            top: '50.5px',
+            padding: '40px 0'
+          }}
+        >
+          <TableOfContents headings={page.data.toc} />
+        </aside>
       </Flex>
     </Flex>
   );
