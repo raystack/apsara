0232244553d561c7855408d09826c6970a168656
Rohan Chakraborty <irohan1999@gmail.com>
2026-01-28 15:08:07 +0530

    feat: migrate avatar to  base ui

--- packages/raystack/components/avatar/index.tsx
+++ packages/raystack/components/avatar/index.tsx
@@ -1,2 +1,2 @@
-export { Avatar, AvatarGroup } from "./avatar";
-export { getAvatarColor } from "./utils";
+export { Avatar, AvatarGroup } from './avatar';
+export { getAvatarColor } from './utils';
