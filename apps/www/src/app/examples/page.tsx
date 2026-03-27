2ab28087ba724f8be928747f8dead1c61290b108
paanSinghCoder <gaurav3017@gmail.com>
2026-03-27 16:17:40 +0530

    refactor: change Sidebar variant from 'inset' to 'floating' and update corresponding CSS styles

--- apps/www/src/app/examples/page.tsx
+++ apps/www/src/app/examples/page.tsx
@@ -86,7 +86,7 @@ const Page = () => {
           backgroundColor: 'var(--rs-color-background-base-primary)'
         }}
       >
-        <Sidebar defaultOpen variant='inset'>
+        <Sidebar defaultOpen variant='floating'>
           <Sidebar.Header>
             <Flex align='center' gap={3}>
               <IconButton
