34197a6bd9542f1dd664767bc36ceea7b6e61210
Gaurav Singh <gaurav3017@gmail.com>
2026-04-07 11:45:54 +0530

    feat (tabs): add variants and sizes (#726)
    * feat: add tabs variants ans sizes
    
    * refactor: update tab structure and content for improved clarity

--- apps/www/src/content/docs/components/tabs/demo.ts
+++ apps/www/src/content/docs/components/tabs/demo.ts
@@ -3,28 +3,20 @@
 export const preview = {
   type: 'code',
   code: `
-  <Flex direction="row" gap="large" style={{ width: "100%", fontSize: "12px" }}>
+  <Flex direction="row" gap="large" style={{ width: "100%", fontSize: "12px", padding: "0 16px" }}>
     <Tabs defaultValue="tab-one">
       <Tabs.List>
-        <Tabs.Tab value="tab-one" leadingIcon={<Info />}>Hoisting</Tabs.Tab>
-        <Tabs.Tab value="tab-two">Hosting</Tabs.Tab>
-        <Tabs.Tab value="tab-three" leadingIcon={<Info />}>Editor</Tabs.Tab>
-        <Tabs.Tab value="tab-four">Billing</Tabs.Tab>
-        <Tabs.Tab value="tab-five">SEO</Tabs.Tab>
+        <Tabs.Tab value="tab-one">Hosting</Tabs.Tab>
+        <Tabs.Tab value="tab-two">Billing</Tabs.Tab>
+        <Tabs.Tab value="tab-three">SEO</Tabs.Tab>
       </Tabs.List>
       <Tabs.Content value="tab-one">
-        <Text>General settings content</Text>
-      </Tabs.Content>
-      <Tabs.Content value="tab-two">
         <Text>Hosting configuration content</Text>
       </Tabs.Content>
+      <Tabs.Content value="tab-two">
+        <Text>Billing preferences content</Text>
+      </Tabs.Content>
       <Tabs.Content value="tab-three">
-        <Text>Editor preferences content</Text>
-      </Tabs.Content>
-      <Tabs.Content value="tab-four">
-        <Text>Billing information content</Text>
-      </Tabs.Content>
-      <Tabs.Content value="tab-five">
         <Text>SEO settings content</Text>
       </Tabs.Content>
     </Tabs>
@@ -76,4 +68,146 @@ export const disabledDemo = {
   <Tabs.Content value="tab2">Disabled tab content</Tabs.Content>
 </Tabs>
 </div>`
+};
+
+export const standaloneVariantDemo = {
+  type: 'code',
+  code: `
+  <div style={{ width: "400px" }}>
+  <Tabs defaultValue="tab1" variant="standalone">
+  <Tabs.List>
+    <Tabs.Tab value="tab1">Account</Tabs.Tab>
+    <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+    <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+  </Tabs.List>
+  <Tabs.Content value="tab1">Account settings</Tabs.Content>
+  <Tabs.Content value="tab2">Password settings</Tabs.Content>
+  <Tabs.Content value="tab3">Other settings</Tabs.Content>
+</Tabs>
+</div>`
+};
+
+export const plainVariantDemo = {
+  type: 'code',
+  code: `
+  <div style={{ width: "400px" }}>
+  <Tabs defaultValue="tab1" variant="plain">
+  <Tabs.List>
+    <Tabs.Tab value="tab1">Account</Tabs.Tab>
+    <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+    <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+  </Tabs.List>
+  <Tabs.Content value="tab1">Account settings</Tabs.Content>
+  <Tabs.Content value="tab2">Password settings</Tabs.Content>
+  <Tabs.Content value="tab3">Other settings</Tabs.Content>
+</Tabs>
+</div>`
+};
+
+export const variantsDemo = {
+  type: 'code',
+  tabs: [
+    {
+      name: 'Segmented (default)',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1">
+      <Tabs.List>
+        <Tabs.Tab value="tab1">Account</Tabs.Tab>
+        <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+        <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Content value="tab1">Account settings</Tabs.Content>
+      <Tabs.Content value="tab2">Password settings</Tabs.Content>
+      <Tabs.Content value="tab3">Other settings</Tabs.Content>
+    </Tabs>
+    </div>`
+    },
+    {
+      name: 'Standalone',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1" variant="standalone">
+      <Tabs.List>
+        <Tabs.Tab value="tab1">Account</Tabs.Tab>
+        <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+        <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Content value="tab1">Account settings</Tabs.Content>
+      <Tabs.Content value="tab2">Password settings</Tabs.Content>
+      <Tabs.Content value="tab3">Other settings</Tabs.Content>
+    </Tabs>
+    </div>`
+    },
+    {
+      name: 'Plain',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1" variant="plain">
+      <Tabs.List>
+        <Tabs.Tab value="tab1">Account</Tabs.Tab>
+        <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+        <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Content value="tab1">Account settings</Tabs.Content>
+      <Tabs.Content value="tab2">Password settings</Tabs.Content>
+      <Tabs.Content value="tab3">Other settings</Tabs.Content>
+    </Tabs>
+    </div>`
+    }
+  ]
+};
+
+export const sizesDemo = {
+  type: 'code',
+  tabs: [
+    {
+      name: 'Small',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1" size="small">
+        <Tabs.List>
+          <Tabs.Tab value="tab1">Account</Tabs.Tab>
+          <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+          <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Content value="tab1">Account settings</Tabs.Content>
+        <Tabs.Content value="tab2">Password settings</Tabs.Content>
+        <Tabs.Content value="tab3">Other settings</Tabs.Content>
+      </Tabs>
+    </div>`
+    },
+    {
+      name: 'Medium',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1" size="medium">
+        <Tabs.List>
+          <Tabs.Tab value="tab1">Account</Tabs.Tab>
+          <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+          <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Content value="tab1">Account settings</Tabs.Content>
+        <Tabs.Content value="tab2">Password settings</Tabs.Content>
+        <Tabs.Content value="tab3">Other settings</Tabs.Content>
+      </Tabs>
+    </div>`
+    },
+    {
+      name: 'Large',
+      code: `
+      <div style={{ width: "400px" }}>
+      <Tabs defaultValue="tab1" size="large">
+        <Tabs.List>
+          <Tabs.Tab value="tab1">Account</Tabs.Tab>
+          <Tabs.Tab value="tab2" disabled>Password</Tabs.Tab>
+          <Tabs.Tab value="tab3">Settings</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Content value="tab1">Account settings</Tabs.Content>
+        <Tabs.Content value="tab2">Password settings</Tabs.Content>
+        <Tabs.Content value="tab3">Other settings</Tabs.Content>
+      </Tabs>
+    </div>`
+    }
+  ]
 };
