12bde1e989a90b15b673c700270a0e24f8de1156
Shreyag02 <shreyagupta72484@gmail.com>
2026-06-08 12:56:36 +0530

    fix(callout): converted dismiss btn to the iconbutton and removed redundant CSS

--- packages/raystack/components/callout/callout.tsx
+++ packages/raystack/components/callout/callout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { InfoCircledIcon } from '@radix-ui/react-icons';
+import { Cross1Icon, InfoCircledIcon } from '@radix-ui/react-icons';
 import { cva, type VariantProps } from 'class-variance-authority';
 import {
   type ComponentProps,
@@ -9,6 +9,7 @@ import {
   useState
 } from 'react';
 
+import { IconButton } from '../icon-button';
 import styles from './callout.module.css';
 
 const callout = cva(styles.callout, {
@@ -101,29 +102,13 @@ export function Callout({
         <div className={styles.actionsContainer}>
           {action && <div className={styles.action}>{action}</div>}
           {dismissible && (
-            <button
-              className={styles.dismiss}
+            <IconButton
+              size={1}
               onClick={handleDismiss}
               aria-label='Dismiss message'
-              type='button'
             >
-              <svg
-                width='12'
-                height='12'
-                viewBox='0 0 12 12'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-                aria-hidden='true'
-                role='presentation'
-              >
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M9.5066 3.3066C9.73115 3.08205 9.73115 2.71798 9.5066 2.49343C9.28205 2.26887 8.91798 2.26887 8.69343 2.49343L6.00001 5.18684L3.3066 2.49343C3.08205 2.26887 2.71798 2.26887 2.49343 2.49343C2.26887 2.71798 2.26887 3.08205 2.49343 3.3066L5.18684 6.00001L2.49343 8.69343C2.26887 8.91798 2.26887 9.28205 2.49343 9.5066C2.71798 9.73115 3.08205 9.73115 3.3066 9.5066L6.00001 6.81318L8.69343 9.5066C8.91798 9.73115 9.28205 9.73115 9.5066 9.5066C9.73115 9.28205 9.73115 8.91798 9.5066 8.69343L6.81318 6.00001L9.5066 3.3066Z'
-                  fill='currentColor'
-                />
-              </svg>
-            </button>
+              <Cross1Icon />
+            </IconButton>
           )}
         </div>
       </div>
